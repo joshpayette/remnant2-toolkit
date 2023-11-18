f8660c528bdd2d50b06c927eacb809a66ae08273
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-11-17 21:05:22 -0500

    Moved build state logic to hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,125 +1,16 @@
 'use client'
 
-import { useCallback, useState } from 'react'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { z } from 'zod'
-import BuildContainer from '@/components/build-container'
-import ArmorItem from '@/components/armor-item'
+import { useState } from 'react'
 import { armorList } from '@/data/armor'
-import type { ArmorSlot, Armor } from '@/data/armor'
+import BuildContainer from '@/components/build-container'
 import ArmorSelect from '@/components/armor-select'
-
-type BuildState = {
-  [key in ArmorSlot]: Armor | null
-}
-
-const initialBuildState: BuildState = {
-  helm: null,
-  torso: null,
-  legs: null,
-  gloves: null,
-  relic: null,
-  amulet: null,
-  ring1: null,
-  ring2: null,
-  ring3: null,
-  ring4: null,
-}
-
-/**
- * Parse the build param from the URL and return the build state
- * @param buildParam The `build` param from the URL
- * @returns
- */
-function getBuildState(buildParam: string | null): BuildState {
-  let buildState = initialBuildState
-  if (buildParam) {
-    const decodedParam = JSON.parse(
-      Buffer.from(buildParam, 'base64').toString('utf-8'),
-    )
-
-    const armorSchema = z
-      .object({
-        name: z.string(),
-        slot: z.string(),
-        path: z.string(),
-      })
-      .nullable()
-
-    // Validate the decodedParam against the armorSchema
-    const parsedParam = z
-      .object({
-        helm: armorSchema,
-        torso: armorSchema,
-        legs: armorSchema,
-        gloves: armorSchema,
-        relic: armorSchema,
-        amulet: armorSchema,
-        ring1: armorSchema,
-        ring2: armorSchema,
-        ring3: armorSchema,
-        ring4: armorSchema,
-      })
-      .safeParse(decodedParam)
-
-    // If the build param is valid, set the build state
-    if (parsedParam.success) {
-      const { data } = parsedParam
-      Object.keys(buildState).forEach((key) => {
-        const slot = key as ArmorSlot
-        const armorForSlot = data[slot]
-        if (armorForSlot) {
-          buildState = { ...buildState, [slot]: armorForSlot }
-        }
-      })
-    } else {
-      console.error('Invalid build param', parsedParam.error)
-    }
-  }
-  return buildState
-}
+import ArmorItem from '@/components/armor-item'
+import useBuildState from '@/hooks/use-build-state'
+import type { ArmorSlot, Armor } from '@/data/armor'
 
 export default function Home() {
+  const { buildState, updateBuildState } = useBuildState()
   const [armorSelectOpen, setArmorSelectOpen] = useState<ArmorSlot | null>(null)
-
-  const router = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  const buildState = getBuildState(searchParams.get('build'))
-
-  /**
-   * Get a new searchParams string by merging the current
-   * searchParams with a provided key/value pair
-   */
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams)
-      params.set(name, value)
-      return params.toString()
-    },
-    [searchParams],
-  )
-
-  /** Update the build state by pushing a new route */
-  const updateBuildState = useCallback(
-    (slot: ArmorSlot, armor: Armor | null) => {
-      const newBuildState = { ...buildState }
-      newBuildState[slot] = armor
-      // Encode the newBuildState as a URL safe base64 string
-      const buildString = Buffer.from(
-        JSON.stringify(newBuildState),
-        'utf-8',
-      ).toString('base64')
-      router.push(pathname + '?' + createQueryString('build', buildString))
-    },
-    [buildState, createQueryString, pathname, router],
-  )
-
-  /** Reset the build state by pushing a new route */
-  const resetBuild = useCallback(() => {
-    router.push(pathname)
-  }, [pathname, router])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
---
+++ src/hooks/use-build-state.ts
@@ -0,0 +1,125 @@
+import { useCallback } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { z } from 'zod'
+import type { ArmorSlot, Armor } from '@/data/armor'
+
+type BuildState = {
+  [key in ArmorSlot]: Armor | null
+}
+
+const initialBuildState: BuildState = {
+  helm: null,
+  torso: null,
+  legs: null,
+  gloves: null,
+  relic: null,
+  amulet: null,
+  ring1: null,
+  ring2: null,
+  ring3: null,
+  ring4: null,
+}
+
+/**
+ * Parse the build param from the URL and return the build state
+ * @param buildParam The `build` param from the URL
+ * @returns
+ */
+function parseBuildState(buildParam: string | null): BuildState {
+  let buildState = initialBuildState
+  if (buildParam) {
+    const decodedParam = JSON.parse(
+      Buffer.from(buildParam, 'base64').toString('utf-8'),
+    )
+
+    const armorSchema = z
+      .object({
+        name: z.string(),
+        slot: z.string(),
+        path: z.string(),
+      })
+      .nullable()
+
+    // Validate the decodedParam against the armorSchema
+    const parsedParam = z
+      .object({
+        helm: armorSchema,
+        torso: armorSchema,
+        legs: armorSchema,
+        gloves: armorSchema,
+        relic: armorSchema,
+        amulet: armorSchema,
+        ring1: armorSchema,
+        ring2: armorSchema,
+        ring3: armorSchema,
+        ring4: armorSchema,
+      })
+      .safeParse(decodedParam)
+
+    // If the build param is valid, set the build state
+    if (parsedParam.success) {
+      const { data } = parsedParam
+      Object.keys(buildState).forEach((key) => {
+        const slot = key as ArmorSlot
+        const armorForSlot = data[slot]
+        if (armorForSlot) {
+          buildState = { ...buildState, [slot]: armorForSlot }
+        }
+      })
+    } else {
+      console.error('Invalid build param', parsedParam.error)
+    }
+  }
+  return buildState
+}
+
+/**
+ * Parses the build state from the encoded URL param, and provides
+ * functions to update the build state in the URL
+ */
+export default function useBuildState() {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const buildState = parseBuildState(searchParams.get('build'))
+
+  /**
+   * Get a new searchParams string by merging the current
+   * searchParams with a provided key/value pair
+   */
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set(name, value)
+      return params.toString()
+    },
+    [searchParams],
+  )
+
+  /** Update the build state by pushing a new route */
+  const updateBuildState = useCallback(
+    (slot: ArmorSlot, armor: Armor | null) => {
+      const newBuildState = { ...buildState }
+      newBuildState[slot] = armor
+      // Encode the newBuildState as a URL safe base64 string
+      const buildString = Buffer.from(
+        JSON.stringify(newBuildState),
+        'utf-8',
+      ).toString('base64')
+      router.push(pathname + '?' + createQueryString('build', buildString))
+    },
+    [buildState, createQueryString, pathname, router],
+  )
+
+  /** Reset the build state by pushing a new route */
+  const resetBuildState = useCallback(() => {
+    router.push(pathname)
+  }, [pathname, router])
+
+  return {
+    buildState,
+    updateBuildState,
+    resetBuildState,
+  }
+}
