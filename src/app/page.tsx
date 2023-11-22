f90bb68b6d4abb1505c4db2623121a611cf98a65
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-11-22 15:51:36 -0500

    Created an ArmorCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-import BuildContainer from '@/components/BuildContainer'
+import ArmorCard from '@/components/ArmorCard'
+import armorList from '@/data/armor-list.json'
+import { ArmorItem } from '@/types/index'
 
 export default function Home() {
+  const item = armorList[0] as ArmorItem
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
-      <BuildContainer />
+      {/* <BuildContainer /> */}
+      <div className="w-[300px]">
+        <ArmorCard item={item} />
+      </div>
     </main>
   )
 }
---
+++ src/components/ArmorCard.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image'
+import { cn } from '@/lib/utils'
+import type { ArmorItem } from '@/types/index'
+
+interface CardProps {
+  item: ArmorItem
+  variant?: 'default' | 'blue'
+}
+
+function CardTitle({ name, slot }: { name: string; slot: string }) {
+  // Capitalize first letter of slot
+  const slotCapitalized = slot.charAt(0).toUpperCase() + slot.slice(1)
+
+  return (
+    <div className="w-full bg-[url('/card-title-bg.jpg')] p-2 text-center">
+      <h3 className="text-2xl text-white">{name}</h3>
+      <p className="text-lg text-[#ff9900]">{slotCapitalized}</p>
+    </div>
+  )
+}
+
+function CardBody({
+  name,
+  path,
+  variant = 'default',
+}: {
+  name: string
+  path: string
+  variant?: CardProps['variant']
+}) {
+  return (
+    <div
+      className={cn(
+        'relative h-[150px] w-full',
+        variant === 'default'
+          ? "bg-[url('/card-body-bg.jpg')]"
+          : "bg-[url('/card-body-bg-blue.jpg')]",
+      )}
+    >
+      <Image
+        src={path}
+        alt={name}
+        fill={true}
+        className="pointer-events-none object-contain group-hover:opacity-75"
+      />
+    </div>
+  )
+}
+
+function CardFooter() {
+  return (
+    <div className="w-full bg-[url('/card-footer-bg.jpg')] p-2 text-center">
+      <p className="text-lg text-white">Footer</p>
+    </div>
+  )
+}
+
+export default function ArmorCard({ item, variant = 'default' }: CardProps) {
+  return (
+    <div className="relative flex w-full flex-col items-center justify-center p-4">
+      <CardTitle name={item.name} slot={item.slot} />
+      <CardBody name={item.name} path={item.path} variant={variant} />
+    </div>
+  )
+}
