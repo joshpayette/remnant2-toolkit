42f5c320afa3211c9ffd7e1fa927ab6c09c2d7ce
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-11-27 18:58:40 -0500

    Fixed Navbar color issue

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -74,7 +74,7 @@ export default function NavBar() {
                   <Link
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-purple-900"
                   >
                     {item.name}
                   </Link>
