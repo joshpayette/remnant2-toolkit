ef0114fe2873cfe4553a5ca61747964e70233c6b
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-03 11:32:12 -0500

    Added close X to Dialog component

--- TODO.md
+++ TODO.md
@@ -3,4 +3,3 @@
 - Finish adding slots to builder
 - Build page can probably all be server-side, allowing for sharing and meta tags
 - Item category collapse/expand status could be in localhost
-- Add closing X to dialog
--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -43,16 +43,33 @@ export default function Dialog({
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <BaseDialog.Panel className="relative transform overflow-hidden border-2 border-green-500 bg-black px-4 pb-4 pt-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:p-6">
-                <div>
-                  <div className="text-center">
-                    <BaseDialog.Title
-                      as="h3"
-                      className="mb-8 text-4xl font-semibold leading-6 text-green-500"
-                    >
-                      {title}
-                    </BaseDialog.Title>
-                    <div className="mt-2">{children}</div>
-                  </div>
+                <button
+                  className="absolute right-0 top-0 p-2 text-white hover:text-green-500"
+                  onClick={onClose}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+                <div className="text-center">
+                  <BaseDialog.Title
+                    as="h3"
+                    className="mb-8 text-4xl font-semibold leading-6 text-green-500"
+                  >
+                    {title}
+                  </BaseDialog.Title>
+                  <div className="mt-2">{children}</div>
                 </div>
               </BaseDialog.Panel>
             </Transition.Child>
