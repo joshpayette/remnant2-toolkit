8aa692590819df15dde01a2a39cb44f9391322ee
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-09 20:48:24 -0500

    Added sizing to BuildButton

--- src/app/builds/(components)/BuildButton.tsx
+++ src/app/builds/(components)/BuildButton.tsx
@@ -1,32 +1,51 @@
 import { cn } from '@/lib/utils'
+import { Fragment } from 'react'
 
 export default function BuildButton({
   onClick,
   children,
   itemName,
   showLabels,
+  size = 'lg',
 }: {
   onClick: () => void
   children: React.ReactNode
   itemName: string | null
   showLabels: boolean
+  size?: 'sm' | 'lg'
 }) {
   return (
-    <button
-      onClick={onClick}
-      className={`mb-4 h-auto min-h-[64px] w-[64px] gap-2 bg-[url('https://${process.env.NEXT_PUBLIC_IMAGE_URL}/card-body-bg.jpg')] flex flex-col items-center justify-center border-2 border-gray-700 hover:border-purple-500`}
+    <div
+      className={cn(
+        'flex items-start justify-start',
+        size === 'lg' && 'flex-col',
+        size === 'sm' && 'flex-row',
+      )}
     >
-      {children}
+      <button
+        onClick={onClick}
+        className={cn(
+          'flex items-center justify-center border-2 border-gray-700 hover:border-purple-500',
+          `bg-[url('https://${process.env.NEXT_PUBLIC_IMAGE_URL}/card-body-bg.jpg')]`,
+          size === 'lg' && 'min-h-[66px] w-[66px]',
+          size === 'sm' && 'min-h-[22px] w-[22px]',
+        )}
+      >
+        {children}
+      </button>
       {showLabels && (
         <div
           className={cn(
-            'p-1 text-[10px] text-white',
+            'flex grow items-center justify-center text-center text-[10px] text-white',
             itemName && 'bg-purple-950',
+            size === 'lg' && 'mb-4 w-[66px] p-1',
+            size === 'sm' &&
+              'mb-0 min-h-[22px] min-w-[22px] border border-black px-1',
           )}
         >
           {itemName}
         </div>
       )}
-    </button>
+    </div>
   )
 }
