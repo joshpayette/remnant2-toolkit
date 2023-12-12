3689f35b9e0eef0a6a399229c3801d3aeb749fef
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-11 22:47:50 -0500

    Added filter to ItemSelect

--- TODO.md
+++ TODO.md
@@ -1,9 +1,9 @@
 # TODO
 
 - Add trait total, limit to 85
-- Add filter to ItemSelect
 - Add share button to copy and paste URL for builder
 - Finish linking items, like guns to their mods, and
   respecting that in the builder.
 - Add text on main page introducing the tool and how it works
   - Add blog functionality for updates on what's new
+- Builder kind of broken on mobile
--- src/app/builder/(components)/ItemSelect.tsx
+++ src/app/builder/(components)/ItemSelect.tsx
@@ -4,6 +4,7 @@ import { type Item, ItemCategory } from '@/types'
 import Dialog from '@/app/(components)/Dialog'
 import ImageBuilderButton from './ImageBuilderButton'
 import { useIsClient } from 'usehooks-ts'
+import { useState } from 'react'
 
 export default function ItemSelect({
   itemList,
@@ -20,6 +21,11 @@ export default function ItemSelect({
 }) {
   const isClient = useIsClient()
 
+  const [filter, setFilter] = useState('')
+  const filteredItemList = itemList.filter((item) =>
+    item.name.toLowerCase().includes(filter.toLowerCase()),
+  )
+
   if (!buildSlot) {
     return null
   }
@@ -35,6 +41,14 @@ export default function ItemSelect({
       title={`Select ${buildSlot}`}
       maxWidthClass="max-w-6xl"
     >
+      <div className="mb-4 flex w-full items-end justify-end">
+        <input
+          type="text"
+          onChange={(e) => setFilter(e.target.value)}
+          className=" text-md rounded border border-green-500 bg-black p-2 text-white outline-none outline-offset-1 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-500"
+          placeholder="Search"
+        />
+      </div>
       <ul
         role="list"
         className="flex flex-wrap items-start justify-center gap-4"
@@ -47,7 +61,7 @@ export default function ItemSelect({
             onClick={() => onSelectItem(null)}
           />
         </li>
-        {itemList.map((item) => (
+        {filteredItemList.map((item) => (
           <li key={item.name} className="mr-2 min-h-[70px] w-[90px]">
             <ImageBuilderButton
               item={item}
