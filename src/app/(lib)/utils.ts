ddf93f477a74ad9909267d881f16f94122e0f4ba
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-16 16:56:37 -0500

    Small refactor

--- src/app/(lib)/utils.ts
+++ src/app/(lib)/utils.ts
@@ -48,21 +48,21 @@ export function toCsv<T extends {}>(data: T[], filename: string) {
  * Converts an Item to a CSV item for export
  */
 export function itemToCsvItem(item: Item): CsvItem {
+  function cleanString(string: string): string {
+    return (
+      string
+        // replace commas with spaces
+        .replaceAll(',', ' ')
+        // replace line breaks with spaces
+        .replace(/(?:\r\n|\r|\n)/g, ' ')
+    )
+  }
+
   return {
     name: item.name,
     category: item.category,
-    description:
-      item.description
-        // replace commas with spaces
-        ?.replaceAll(',', ' ')
-        // replace line breaks with spaces
-        .replace(/(?:\r\n|\r|\n)/g, ' ') || '',
-    howToGet:
-      item.howToGet
-        // replace commas with spaces
-        ?.replaceAll(',', ' ')
-        // replace line breaks with spaces
-        .replace(/(?:\r\n|\r|\n)/g, ' ') || '',
+    description: cleanString(item.description || ''),
+    howToGet: cleanString(item.howToGet || ''),
     wikiLinks: item.wikiLinks?.join('; ') || '',
   }
 }
