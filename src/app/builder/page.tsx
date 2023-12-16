c63739f17514fbe85f34dda98493cd29caa00792
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-16 14:50:49 -0500

    Small update to build disclaimer

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -61,12 +61,12 @@ export default function BuildHomePage() {
       >
         <div
           id="alert"
-          className="rounded border border-green-500 bg-black p-4 text-green-500"
+          className="rounded border border-yellow-500 bg-black p-4 text-yellow-500"
         >
           <p>
             This tool should be mostly stable at this point. While I will try to
-            avoid breaking changes, they are always possible. Use the export to
-            CSV button to save your build data in case.
+            avoid breaking changes, they are always possible. Be sure to export
+            your builds if you want to save them.
           </p>
         </div>
       </PageHeader>
