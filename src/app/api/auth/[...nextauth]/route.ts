de7242ede834157ea5de60f545923e5bf4bda182
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-20 13:22:37 -0500

    Added additional auth providers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import DiscordProvider from 'next-auth/providers/discord'
+import GoogleProvider from 'next-auth/providers/google'
+import RedditProvider from 'next-auth/providers/reddit'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 
@@ -11,6 +13,19 @@ const handler = NextAuth({
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID ?? '',
       clientSecret: process.env.DISCORD_CLIENT_SECRET ?? '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID ?? '',
+      clientSecret: process.env.REDDIT_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          duration: 'permanent',
+        },
+      },
     }),
   ],
 })
