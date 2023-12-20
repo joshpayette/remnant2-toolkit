71b24bd1c7e6c318735992d01ebd451958f76b67
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-20 09:27:31 -0500

    Added nextauth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth'
+import DiscordProvider from 'next-auth/providers/discord'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID ?? '',
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? '',
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
