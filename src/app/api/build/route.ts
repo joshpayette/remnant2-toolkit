e11c2f8fde79ebaaa6563e6f981368d5d6893e18
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-22 13:25:18 -0500

    Build fetches from the database

--- src/app/api/build/route.ts
+++ src/app/api/build/route.ts
@@ -7,6 +7,62 @@ import { TraitItem } from '@/app/(types)/TraitItem'
 import { WeaponItem } from '@/app/(types)/WeaponItem'
 import { Build } from '@prisma/client'
 
+export async function GET(request: Request) {
+  console.info('Reached this route!')
+
+  if (!request) {
+    console.error('No request body!')
+    return Response.json({ message: 'No request body!' }, { status: 500 })
+  }
+
+  const body = await request.json()
+  console.info('body', body)
+  const { buildId } = body
+  if (!buildId) {
+    console.error('No buildId provided!')
+    return Response.json({ message: 'No buildId provided!' }, { status: 500 })
+  }
+
+  const build = await prisma?.build.findUnique({
+    where: {
+      id: buildId,
+    },
+    include: {
+      createdBy: true,
+    },
+  })
+  if (!build) {
+    console.error('Build not found!')
+    return Response.json({ message: 'Build not found!' }, { status: 404 })
+  }
+
+  if (build.public) {
+    console.info('build', build)
+    return Response.json({ build }, { status: 200 })
+  }
+
+  const session = await getServerSession()
+  if (!session || !session.user || build.createdBy.id !== session.user.id) {
+    console.error(
+      'You must be logged in as the build creator to view a private build.',
+    )
+    return Response.json(
+      {
+        message:
+          'You must be logged in as the build creator to view a private build.',
+      },
+      { status: 401 },
+    )
+  }
+
+  console.info('build', build)
+
+  return Response.json(
+    { message: 'Successfully fetched build!', build },
+    { status: 200 },
+  )
+}
+
 export async function POST(request: Request) {
   const session = await getServerSession()
   if (!session || !session.user) {
--- src/app/builder/[id]/page.tsx
+++ src/app/builder/[id]/page.tsx
@@ -1,6 +1,47 @@
-async function getBuild(id: string) {
-  const res = await fetch(`/api/builds/${id}`)
-  return res.json()
+import { getServerSession } from 'next-auth'
+import { prisma } from '@/app/(lib)/db'
+
+async function getBuild(buildId: string) {
+  if (!buildId) {
+    console.error('No buildId provided!')
+    return Response.json({ message: 'No buildId provided!' }, { status: 500 })
+  }
+
+  const build = await prisma.build.findUnique({
+    where: {
+      id: buildId,
+    },
+    include: {
+      createdBy: true,
+    },
+  })
+  if (!build) {
+    console.error('Build not found!', build)
+    return Response.json({ message: 'Build not found!' }, { status: 404 })
+  }
+
+  if (build.public) {
+    return Response.json({ build }, { status: 200 })
+  }
+
+  const session = await getServerSession()
+  if (!session || !session.user || build.createdBy.id !== session.user.id) {
+    console.error(
+      'You must be logged in as the build creator to view a private build.',
+    )
+    return Response.json(
+      {
+        message:
+          'You must be logged in as the build creator to view a private build.',
+      },
+      { status: 401 },
+    )
+  }
+
+  return Response.json(
+    { message: 'Successfully fetched build!', build },
+    { status: 200 },
+  )
 }
 
 export default async function Page({
@@ -8,13 +49,20 @@ export default async function Page({
 }: {
   params: { id: string }
 }) {
-  const buildData = getBuild(id)
-  const build = await buildData
+  const buildData = await getBuild(id)
+  if (buildData.status !== 200) {
+    return (
+      <div>
+        <h1>Build {id} not found!</h1>
+      </div>
+    )
+  }
+  const { build } = await buildData.json()
 
   return (
     <div>
       <h1>Build {id}</h1>
-      <pre>{JSON.stringify(getBuild(id), null, 2)}</pre>
+      <pre>{JSON.stringify(build)}</pre>
     </div>
   )
 }
