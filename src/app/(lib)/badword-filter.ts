f263a3d0aba4b6fdf66db71770771473bb5d4acc
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-23 20:49:41 -0500

    Added bad word filter

---
+++ src/app/(lib)/badword-filter.ts
@@ -0,0 +1,7 @@
+import Filter from 'bad-words'
+
+export const badwordFilter = new Filter()
+// filter.addWords('some', 'bad', 'words')
+// filter.removeWords('some', 'bad', 'words')
+
+export default badwordFilter
