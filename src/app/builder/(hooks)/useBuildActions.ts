1d8ab0e8e6babcb865f213040f2232e094e58bc3
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2023-12-26 22:17:19 -0500

    Duplicating a build now adds (copy) to name

--- src/app/builder/(hooks)/useBuildActions.ts
+++ src/app/builder/(hooks)/useBuildActions.ts
@@ -32,7 +32,11 @@ export default function useBuildActions() {
   }
 
   function handleDuplicateBuild(buildState: BuildState) {
-    const editBuildUrl = buildToQueryParams(buildState)
+    const newBuildName = `${buildState.name} (copy)`
+    const editBuildUrl = buildToQueryParams({
+      ...buildState,
+      name: newBuildName,
+    })
 
     setBuilderStorage({
       ...builderStorage,
