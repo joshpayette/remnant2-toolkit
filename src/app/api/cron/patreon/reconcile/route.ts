f2b75fb999c7a1d30e3bd61b0678ed50843b8adc
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-01-01 17:18:55 -0500

    Turned on restricted access to cron script

--- src/app/api/cron/patreon/reconcile/route.ts
+++ src/app/api/cron/patreon/reconcile/route.ts
@@ -17,12 +17,12 @@ const allowListIserIds = [
  * CRON script that runs to moderate reported users and builds
  */
 export async function GET(request: NextRequest) {
-  // const authHeader = request.headers.get('authorization')
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return new Response('Unauthorized', {
-  //     status: 401,
-  //   })
-  // }
+  const authHeader = request.headers.get('authorization')
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    })
+  }
 
   try {
     const patreonAPIClient = patreonAPI(
