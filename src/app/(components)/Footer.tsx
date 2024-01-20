75c336837beec7963d28c39ae23cf4c997a5e616
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-01-20 12:48:58 -0500

    Added Discord link

--- src/app/(components)/Footer.tsx
+++ src/app/(components)/Footer.tsx
@@ -7,7 +7,17 @@ export default function Footer() {
   return (
     <div className="max-w-2xl gap-2">
       <div className="flex w-full flex-row items-center justify-center gap-4 p-2">
-        <a
+        <a href="https://discord.gg/kgVaU3zAQ7" target="_blank">
+          <Image
+            src={`https://${process.env.NEXT_PUBLIC_IMAGE_URL}/discord.png`}
+            alt="Donate with PayPal"
+            width={32}
+            height={32}
+            className="h-6 w-6"
+            loading="eager"
+          />
+        </a>
+        {/* <a
           href="https://github.com/joshpayette/remnant2-toolkit"
           target="_blank"
         >
@@ -19,7 +29,7 @@ export default function Footer() {
             className="h-8 w-8"
             loading="eager"
           />
-        </a>
+        </a> */}
         <a href="https://twitter.com/Remnant2Toolkit" target="_blank">
           <Image
             src={`https://${process.env.NEXT_PUBLIC_IMAGE_URL}/twitter.png`}
