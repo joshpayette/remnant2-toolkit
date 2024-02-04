2de30e927b901d17bf14aa5c727380a35bf3b8b2
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-04 14:50:00 -0500

    Actually resolved some css errors now

--- src/features/build/components/PopularBuildBadge.tsx
+++ src/features/build/components/PopularBuildBadge.tsx
@@ -7,12 +7,15 @@ export default function PopularBuildBadge() {
     <Tooltip
       content={`Awarded to builds that exceed ${POPULAR_VOTE_THRESHOLD} favorites!`}
     >
-      <Image
-        src={`https://${process.env.NEXT_PUBLIC_IMAGE_URL}/badges/popular_build_badge.png`}
-        width={43}
-        height={50}
-        alt="image denoting the build is popular"
-      />
+      <button>
+        <Image
+          src={`https://${process.env.NEXT_PUBLIC_IMAGE_URL}/badges/popular_build_badge.png`}
+          width={50}
+          height={50}
+          alt="image denoting the build is popular"
+          className="h-[50px] w-[50px]"
+        />
+      </button>
     </Tooltip>
   )
 }
