c6becb78dd3fcaf180e655f7be53b7cea338ebd6
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-06 16:26:06 -0500

    Update metadata.ts

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -58,8 +58,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: 'https://d2sqltdcj8czo5.cloudfront.net/og_image.png',
-        width: 200,
-        height: 200,
+        width: 100,
+        height: 100,
       },
     ],
   },
