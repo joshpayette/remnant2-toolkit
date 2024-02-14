cd76a32c493b01cc487ca57004d7d9b89a2cb556
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-14 15:39:44 -0500

    Added base data files for remnant enemies

---
+++ src/features/enemies/data/remnantEnemies.ts
@@ -0,0 +1,13 @@
+import { Enemy } from '../types'
+
+export const remnantEnemies: Enemy[] = [
+  {
+    id: 'bp8Z4T',
+    name: 'Arcanum Diviner Aberration',
+    category: 'aberration',
+    location: 'losomn',
+    wikiLinks: [],
+    fireResistance: 10,
+    acidResistance: 10,
+  },
+]
---
+++ src/features/enemies/types.ts
@@ -0,0 +1,14 @@
+export interface Enemy {
+  id: string
+  name: string
+  category: 'enemy' | 'boss' | 'world boss' | 'aberration'
+  location: 'losomn' | `n'erud` | 'yaesha' | 'labyrinth' | 'root earth'
+  dungeon?: string
+  wikiLinks: string[]
+  bleedResistance?: number
+  fireResistance?: number
+  shockResistance?: number
+  acidResistance?: number
+  meleeResistance?: number
+  notes?: string
+}
