306545bddf068542ede4a2f12fab64646b70be30
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-16 16:32:53 -0500

    Additional layout fix

--- src/features/items/components/MasonryItemList.tsx
+++ src/features/items/components/MasonryItemList.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { Masonry } from 'masonic'
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import { useIsClient } from 'usehooks-ts'
+
+import { getArrayOfLength } from '@/features/build/lib/getArrayOfLength'
+import { Skeleton } from '@/features/ui/Skeleton'
 
 import { Item } from '../types'
 import { ItemInfoDialog } from './ItemInfoDialog'
@@ -22,7 +25,7 @@ export function MasonryItemList({ items, label = 'Items' }: Props) {
     setSelectedItem(item)
   }
 
-  if (!isClient) return null
+  if (!isClient) return <Loading />
   if (items.length === 0) return null
 
   return (
@@ -32,24 +35,36 @@ export function MasonryItemList({ items, label = 'Items' }: Props) {
         open={infoOpen}
         onClose={() => setSelectedItem(null)}
       />
+      <Suspense fallback={<Loading />}>
+        <div className="flex h-[500px] w-full flex-col items-center justify-center overflow-auto p-4 sm:h-[1000px]">
+          <h2 className="my-4 text-4xl font-bold text-green-500">{label}</h2>
 
-      <div className="flex w-full flex-col items-center justify-center p-4">
-        <h2 className="my-4 text-4xl font-bold text-green-500">{label}</h2>
-        <Masonry
-          key={new Date().getTime()}
-          items={items}
-          render={({ index, data, width }) => (
-            <MasonryCard
-              index={index}
-              data={data}
-              width={width}
-              onMoreInfoClick={handleMoreInfoClick}
-            />
-          )}
-          columnGutter={8}
-          rowGutter={8}
-        />
-      </div>
+          <Masonry
+            key={new Date().getTime()}
+            items={items}
+            render={({ index, data, width }) => (
+              <MasonryCard
+                index={index}
+                data={data}
+                width={width}
+                onMoreInfoClick={handleMoreInfoClick}
+              />
+            )}
+            columnGutter={8}
+            rowGutter={8}
+          />
+        </div>
+      </Suspense>
     </>
+  )
+}
+
+function Loading() {
+  return (
+    <div className="flex h-[500px] w-full flex-row flex-wrap items-center justify-center gap-4 overflow-auto p-4 sm:h-[1000px]">
+      {getArrayOfLength(10).map((_, i) => (
+        <Skeleton key={i} className="h-[350px] w-[250px]" />
+      ))}
+    </div>
   )
 }
