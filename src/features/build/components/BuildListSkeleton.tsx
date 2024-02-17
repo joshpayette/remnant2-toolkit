e9f79baefa80ebe2a3f026a1f6cd699ad3f71d3e
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-17 10:49:16 -0500

    Update BuildListSkeleton.tsx

--- src/features/build/components/BuildListSkeleton.tsx
+++ src/features/build/components/BuildListSkeleton.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function BuildListSkeleton({ itemsPerPage }: Props) {
   return getArrayOfLength(itemsPerPage).map((_, index) => (
     <div key={index} className="h-full w-full">
-      <Skeleton key={index} className="h-[359px] w-full" />
+      <Skeleton key={index} className="h-[357px] w-full" />
     </div>
   ))
 }
