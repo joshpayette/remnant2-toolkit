b5fb0e17334acd7b4f6d95e70312d87d24e87162
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-21 11:40:59 -0500

    Disabled speed insights, gleamed what I needed from it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import './globals.css'
 import 'react-toastify/dist/ReactToastify.css'
 
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Viewport } from 'next'
 import { Suspense } from 'react'
 import { ToastContainer } from 'react-toastify'
@@ -75,7 +74,7 @@ export default async function RootLayout({
           </Suspense>
         </SessionProvider>
         <Analytics />
-        <SpeedInsights />
+        {/* <SpeedInsights /> */}
       </body>
     </html>
   )
