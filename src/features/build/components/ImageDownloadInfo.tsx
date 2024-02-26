422d4ceb6300002bfa869e54dd7f9339668018f6
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-26 16:01:21 -0500

    Update ImageDownloadInfo.tsx

--- src/features/build/components/ImageDownloadInfo.tsx
+++ src/features/build/components/ImageDownloadInfo.tsx
@@ -20,7 +20,6 @@ export function ImageDownloadInfo({ imageDownloadInfo, onClose }: Props) {
       onClose={onClose}
     >
       <div className="flex w-full flex-col items-start justify-center gap-y-4 text-left text-sm">
-        <p>Left-click the image to save it to your device.</p>
         <p>Right-click the image to copy the image and paste it elsewhere.</p>
         <p className="text-red-500">
           Warning: Do not choose{' '}
@@ -28,14 +27,7 @@ export function ImageDownloadInfo({ imageDownloadInfo, onClose }: Props) {
           <span className="font-bold">Copy Image Address</span>, as it could
           crash your browser due to the large data URL of the image.
         </p>
-        {/* <a
-          href={imageLink}
-          target="_blank"
-          download={imageName}
-          className="text-green-500 hover:text-green-300 hover:underline"
-        > */}
         <img src={imageLink} alt="Your Build Image" />
-        {/* </a> */}
       </div>
     </Dialog>
   )
