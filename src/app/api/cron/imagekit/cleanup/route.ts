58504f0af490d70aeb1e2727d43eccaf3de5b5dc
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-02-28 18:46:05 -0500

    Update route.ts

--- src/app/api/cron/imagekit/cleanup/route.ts
+++ src/app/api/cron/imagekit/cleanup/route.ts
@@ -19,7 +19,7 @@ export async function GET(request: NextRequest) {
     // Get all files created over an hour ago
     const files = await imagekit.listFiles({
       path: 'build-uploads',
-      searchQuery: 'updatedAt > 1h',
+      searchQuery: 'updatedAt > "1h"',
     })
 
     const fileIds = files.map((file) => file.fileId)
