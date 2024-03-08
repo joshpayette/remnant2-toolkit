0564d23cd732f2a2c56ac282a5618a266810095b
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-03-08 14:44:40 -0500

    Updated button colors

--- src/features/bug-reports/ReportBugButton.tsx
+++ src/features/bug-reports/ReportBugButton.tsx
@@ -9,7 +9,7 @@ export function ReportBugButton() {
   return (
     <div className="flex items-center justify-start">
       <button
-        className="bg-primary-500 hover:border-primary-300 flex w-auto items-center justify-center gap-1 rounded-md border-2 border-black p-2 text-sm font-bold text-black drop-shadow-lg"
+        className="bg-accent2-500 hover:border-accent2-300 flex w-auto items-center justify-center gap-1 rounded-md border-2 border-black p-2 text-sm font-bold text-black drop-shadow-lg"
         aria-label="Report a bug"
         onClick={async () => {
           const report = prompt('Please describe the bug')
