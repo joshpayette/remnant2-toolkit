c8cb52c56daa83dba5e3df5cf88ce7578fd15895
Aleksander Lis <158191760+AlexijSeptimus@users.noreply.github.com>
2024-03-13 20:55:48 +0000

    fixed spelling

--- src/features/items/constants.ts
+++ src/features/items/constants.ts
@@ -188,14 +188,14 @@ export const DESCRIPTION_TAGS = [
   },
   {
     type: 'Crit Damage',
-    token: 'Crit Damage',
-    color: 'text-red-400',
+    token: 'Crit damage',
+    color: 'text-red-500',
     description: 'Base 50%. Critical damage is multiplicative with different sources of damage.',
   },
   {
     type: 'Critical Damage',
-    token: 'Critical Damage',
-    color: 'text-red-400',
+    token: 'Critical damage',
+    color: 'text-red-500',
     description: 'Base 50%. Critical damage is multiplicative with different sources of damage.',
   },
   {
