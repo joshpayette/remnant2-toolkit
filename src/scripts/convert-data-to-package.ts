fd184657b3089f96d65a6519611e926b70911627
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-03-15 20:02:26 -0400

    Removed unneeded files

--- src/scripts/convert-data-to-package.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This script will be used to convert the local data to a format that
- * can be used in the remnant2toolkit/data npm package
- */
-
-import { remnantItems } from '../features/items/data/remnantItems'
-
-// TODO
