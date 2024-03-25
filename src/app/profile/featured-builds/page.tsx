4a1c4cf1fc034de867bae7ab25716bd6a93b465e
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-03-24 23:46:57 -0400

    Replaced server errors with UI errors

--- src/app/profile/featured-builds/page.tsx
+++ src/app/profile/featured-builds/page.tsx
@@ -1,11 +1,17 @@
 import { permanentRedirect } from 'next/navigation'
 
 import { getServerSession } from '@/features/auth/lib'
+import { PageHeader } from '@/features/ui/PageHeader'
 
 export default async function Page() {
   const session = await getServerSession()
   if (!session || !session.user) {
-    throw new Error('User not found at /profile/favorited-builds/page')
+    return (
+      <PageHeader
+        title="Login Required"
+        subtitle="You must be logged in to view this page"
+      />
+    )
   }
 
   permanentRedirect(`/profile/${session?.user?.id}/favorited-builds`)
