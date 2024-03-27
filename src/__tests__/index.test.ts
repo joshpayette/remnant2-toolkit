a95c8c631e770648105779ee69612a353f770355
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-03-27 19:28:37 -0400

    Added more avatars

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,6 @@
 /* eslint-env jest */
 
+import { additionalAvatars } from '@/app/profile/[userId]/(lib)/additionalAvatars'
 import { remnantEnemies } from '@/features/enemies/remnantEnemies'
 import { allItems } from '@/features/items/data/allItems'
 
@@ -30,6 +31,22 @@ describe('Enemies', () => {
     const newEnemy = { ...remnantEnemies[0] }
     remnantEnemies.push(newEnemy)
     const newIds = remnantEnemies.map((enemy) => enemy.id)
+    const newUniqueIds = new Set(newIds)
+    expect(newUniqueIds.size).not.toBe(newIds.length)
+  })
+})
+
+describe('Avatars', () => {
+  it('all should have unique ids', () => {
+    const ids = additionalAvatars.map((avatar) => avatar.id)
+    const uniqueIds = new Set(ids)
+    expect(uniqueIds.size).toBe(ids.length)
+
+    // Insert a new avatar with the same id as the first avatar
+    // and check that the ids are no longer unique
+    const newAvatar = { ...additionalAvatars[0] }
+    additionalAvatars.push(newAvatar)
+    const newIds = additionalAvatars.map((avatar) => avatar.id)
     const newUniqueIds = new Set(newIds)
     expect(newUniqueIds.size).not.toBe(newIds.length)
   })
--- src/app/profile/[userId]/(lib)/constants.ts
+++ src/app/profile/[userId]/(lib)/constants.ts
@@ -1,5 +1,6 @@
 import { image } from 'html2canvas/dist/types/css/types/image'
 
+import { additionalAvatars } from '@/app/profile/[userId]/(lib)/additionalAvatars'
 import { remnantEnemies } from '@/features/enemies/remnantEnemies'
 import { amuletItems } from '@/features/items/data/amuletItems'
 import { armorItems } from '@/features/items/data/armorItems'
@@ -29,129 +30,6 @@ const amuletImages = amuletItems
     imagePath: item.imagePath as string,
   }))
 
-const additionalImages = [
-  {
-    id: 'AD7cnH',
-    name: 'Dran Nurse',
-    imagePath: '/avatars/dran_nurse.jpg',
-  },
-  {
-    id: '4a5EAU',
-    name: 'Bridgekeeper Facepalm',
-    imagePath: '/avatars/bridgekeeper_facepalm.jpg',
-  },
-  {
-    id: '7dG8vW',
-    name: 'Nimue Sleeping',
-    imagePath: '/avatars/nimue_sleeping.jpg',
-  },
-  {
-    id: 'yi3JVd',
-    name: 'Reggie',
-    imagePath: '/avatars/reggie.jpg',
-  },
-  {
-    id: 'cU86w5',
-    name: 'Andrew Ford',
-    imagePath: '/avatars/andrew_ford.jpg',
-  },
-  {
-    id: 'aPqs77',
-    name: 'Ava McCabe',
-    imagePath: '/avatars/ava_mccabe.jpg',
-  },
-  {
-    id: 'DmWRA8',
-    name: 'Bedel of the Vaunnt',
-    imagePath: '/avatars/bedel_of_the_vaunnt.jpg',
-  },
-  {
-    id: 'mjtk8P',
-    name: 'Bloodmoon Altar',
-    imagePath: '/avatars/bloodmoon_altar.jpg',
-  },
-  {
-    id: 'sox94R',
-    name: 'Bo',
-    imagePath: '/avatars/bo.jpg',
-  },
-  {
-    id: 'V8Gi2g',
-    name: 'Brabus',
-    imagePath: '/avatars/brabus.jpg',
-  },
-  {
-    id: 'frboD3',
-    name: 'Bridge Warden',
-    imagePath: '/avatars/bridge_warden.jpg',
-  },
-  {
-    id: 'i78SMB',
-    name: 'Cass',
-    imagePath: '/avatars/cass.jpg',
-  },
-  {
-    id: '6UuaBk',
-    name: 'Clementine',
-    imagePath: '/avatars/clementine.jpg',
-  },
-  {
-    id: 'DX77uX',
-    name: 'Don "Rigs" Rigler',
-    imagePath: '/avatars/don_rigs_rigler.jpg',
-  },
-  {
-    id: '2dR2wb',
-    name: 'Dr. Norah',
-    imagePath: '/avatars/dr_norah.jpg',
-  },
-  {
-    id: '4KtJsH',
-    name: 'Dran in the Sewer',
-    imagePath: '/avatars/dran_in_the_sewer.jpg',
-  },
-  {
-    id: 'agQS6g',
-    name: 'Dran Oracle',
-    imagePath: '/avatars/dran_oracle.jpg',
-  },
-  {
-    id: 'w8m6tA',
-    name: 'Dran Preacher',
-    imagePath: '/avatars/dran_preacher.jpg',
-  },
-  {
-    id: 'P9ufkJ',
-    name: 'Drzyr Replicator',
-    imagePath: '/avatars/drzyr_replicator.jpg',
-  },
-  {
-    id: 'PSya6k',
-    name: 'Duane',
-    imagePath: '/avatars/duane.jpg',
-  },
-  {
-    id: 'dLMc7Z',
-    name: 'Dwell',
-    imagePath: '/avatars/dwell.jpg',
-  },
-  {
-    id: 'AXavV7',
-    name: 'Earl',
-    imagePath: '/avatars/earl.jpg',
-  },
-  {
-    id: 'x2nDCa',
-    name: 'Elowen',
-    imagePath: '/avatars/elowen.jpg',
-  },
-  {
-    id: 'EuqP9e',
-    name: 'Feast Master',
-    imagePath: '/avatars/feast_master.jpg',
-  },
-]
-
 /**
  * The list of avatars that a user can choose from
  */
@@ -159,7 +37,7 @@ export const AVATARS = [
   ...enemyImages,
   ...helmImages,
   ...amuletImages,
-  ...additionalImages,
+  ...additionalAvatars,
 ].sort((a, b) => a.name.localeCompare(b.name)) satisfies Array<{
   id: string
   name: string
