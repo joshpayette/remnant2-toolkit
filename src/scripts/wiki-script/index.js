e83e9763bef3821278a0acf6c02fb204aaa48598
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-04-02 20:46:46 -0400

    Updated wiki script

--- src/scripts/wiki-script/index.js
+++ src/scripts/wiki-script/index.js
@@ -3,6 +3,15 @@ const path = require('path')
 
 const inputFolder = path.join(__dirname, './input')
 const outputFolder = path.join(__dirname, './output')
+const noDataOutputFolder = path.join(outputFolder, 'nodata')
+
+// create folders if they don't exist
+if (!fs.existsSync(outputFolder)) {
+  fs.mkdirSync(outputFolder)
+}
+if (!fs.existsSync(noDataOutputFolder)) {
+  fs.mkdirSync(noDataOutputFolder)
+}
 
 const files = fs.readdirSync(inputFolder)
 
@@ -18,7 +27,21 @@ files.forEach((file) => {
   for (const key in data) {
     const value = data[key]
     if (value.Type === 'HitLogComponent') {
-      bodySegments = value.Properties['']
+      // If not hitlocations, write text to point to Template.ObjectPath
+      if (!value.Properties) {
+        const objectPath = value.Template.ObjectPath
+        const csvContent = `see: ${objectPath} for hit locations`
+        console.info(`Writing ${file}.csv file...`)
+        const outputFilePath = path.join(
+          outputFolder,
+          'nodata',
+          `${file.replace('.json', '')}.csv`,
+        )
+        fs.writeFileSync(outputFilePath, csvContent)
+        console.info(`File ${file}.csv written successfully!`)
+        return
+      }
+      bodySegments = value.Properties.HitLocations
       break
     }
   }
@@ -33,12 +56,21 @@ files.forEach((file) => {
       DamageScalar: segment.DamageScalar,
       HealthRatio: segment.HealthRatio,
       EventOnDestroyed: segment.EventOnDestroyed,
+      EventOnRepaired: segment.EventOnRepaired,
+      KillOnDestroyed: segment.KillOnDestroyed,
+      DestroyedAnimTag: segment.DestroyAnimTag,
+      DebrisSocketLocationOnDestroy: segment.DebrisSocketLocationOnDestroy,
+      DebrisOnDestroy: segment.DebrisOnDestroy,
+      bResistSpot: segment.bResistSpot,
       bIsWeakSpot: segment.bIsWeakSpot,
+      AoEPriority: segment.AoEPriority,
+      bAoERequiresLoS: segment.bAoERequiresLoS,
+      DestroyedDependentHitLocationsToActivate:
+        segment.DestroyedDependentHitLocationsToActivate,
+      BoneNamesToActivateCollision: segment.BoneNamesToActivateCollision,
     }
     dataToWrite.push(dataToPush)
   }
-
-  console.info('dataToWrite', dataToWrite)
 
   // Write the dataToWrite to a new CSV file
   console.info(`Writing ${file}.xlsx file...`)
@@ -46,12 +78,16 @@ files.forEach((file) => {
     outputFolder,
     `${file.replace('.json', '')}.csv`,
   )
-  const header =
-    'NameID,PhysMat_ObjectName,PhysMat_ObjectPath,bCollisionInitiallyEnabled,DamageScalar,HealthRatio,EventOnDestroyed,bIsWeakSpot\n'
+
+  const header = `NameID,PhysMat_ObjectName,PhysMat_ObjectPath,bCollisionInitiallyEnabled,DamageScalar,HealthRatio,EventOnDestroyed,EventOnRepaired,KillOnDestroyed,DestroyedAnimTag, DebrisSocketLocationOnDestroy,DebrisOnDestroy,bResistSpot,bIsWeakSpot,AoEPriority,bAoERequiresLoS,DestroyedDependentHitLocationsToActivate,BoneNamesToActivateCollision\n`
+
   let csvContent = header
-  dataToWrite.forEach((data) => {
-    csvContent += `${data.NameID},${data.PhysMat},${data.bCollisionInitiallyEnabled},${data.DamageScalar},${data.HealthRatio},${data.EventOnDestroyed},${data.blsWeakSpot}\n`
-  })
+  csvContent += dataToWrite
+    .map(
+      (data) =>
+        `${data.NameID},${data.PhysMat_ObjectName},${data.PhysMat_ObjectPath},${data.bCollisionInitiallyEnabled},${data.DamageScalar},${data.HealthRatio},${data.EventOnDestroyed},${data.EventOnRepaired},${data.KillOnDestroyed},${data.DestroyedAnimTag},${data.DebrisSocketLocationOnDestroy},${data.DebrisOnDestroy},${data.bResistSpot},${data.bIsWeakSpot},${data.AoEPriority}, ${data.bAoERequiresLoS},${data.DestroyedDependentHitLocationsToActivate},${data.BoneNamesToActivateCollision}`,
+    )
+    .join('\n')
   fs.writeFileSync(outputFilePath, csvContent)
   console.info(`File ${file}.csv written successfully!`)
 })
