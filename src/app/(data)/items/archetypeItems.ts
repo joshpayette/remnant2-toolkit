b6c1ed0a833e9c762ed9d06434a5955acbf1a4a4
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-04-18 19:37:42 -0400

    Update archetypeItems.ts

--- src/app/(data)/items/archetypeItems.ts
+++ src/app/(data)/items/archetypeItems.ts
@@ -262,7 +262,7 @@ export const archetypeItems: ArchetypeItem[] = [
   {
     category: 'archetype',
     name: 'Invoker',
-    imagePath: '/archtype/invoker-placeholder.png', // TODO
+    imagePath: '/archtype/invoker-placeholder1.png', // TODO
     id: 'Y2NhrX',
     dlc: 'dlc2',
     description: 'NO DESCRIPTION YET', // TODO
