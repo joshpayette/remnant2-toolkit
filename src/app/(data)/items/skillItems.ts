353a91734ad4747d96ef64c13669a524dbfc6d6d
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-04-18 13:11:42 -0400

    Update skillItems.ts

--- src/app/(data)/items/skillItems.ts
+++ src/app/(data)/items/skillItems.ts
@@ -642,11 +642,13 @@ export const skillItems: SkillItem[] = [
   {
     category: 'skill',
     name: 'Way of Kaeula',
-    imagePath: '/skill/way-of-kaeula-placeholder.png', // TODO
+    imagePath: '/skill/way-of-kaeula-placeholder1.png', // TODO
     id: 'h4B9dD',
     dlc: 'dlc2',
     tags: [],
-    description: 'NO DESCRIPTION YET', // TODO
+    description: `PRERELEASE Invoke Kaeula to cast a Tidal Wave, dealing 517.6 Elemental damage and conjuring a 30m Rainstorm for 19.5s. Allies inside Rainstorm gain HASTE. Enemies inside Rainstorm gain DRENCHED for 10s.
+    DRENCHED: Reduces Movement Speed by 10%. Every few seconds, lightning strikes enemies dealing 1,035.3 base SHOCK damage split among DRENCHED targets.`,
+    cooldown: 112.8,
     wikiLinks: [],
     linkedItems: {
       archetype: {
@@ -657,11 +659,13 @@ export const skillItems: SkillItem[] = [
   {
     category: 'skill',
     name: 'Way of Meidra',
-    imagePath: '/skill/way-of-meidra-placeholder.png',
+    imagePath: '/skill/way-of-meidra-placeholder1.png',
     id: '7DnBmE',
     dlc: 'dlc2',
     tags: [],
-    description: 'NO DESCRIPTION YET', // TODO
+    description: `Invoke Meidra to heal all allies for 20% Max Health over 1.5s and conjure a 20m Forest Growth for 19.5s. After fully blooming, allies in the Forest Growth heal 2% Max Health per second and gain 3% of base damage dealt as Lifesteal. Enemies inside Forest Growth gain GLOOM for 10s.
+    GLOOM: Increases incoming Elemental damage by 15%.`,
+    cooldown: 112.8,
     wikiLinks: [],
     linkedItems: {
       archetype: {
@@ -672,11 +676,14 @@ export const skillItems: SkillItem[] = [
   {
     category: 'skill',
     name: 'Way of Lydusa',
-    imagePath: '/skill/way-of-lydusa-placeholder.png', // TODO
+    imagePath: '/skill/way-of-lydusa-placeholder1.png', // TODO
     id: 'T2xA6c',
     dlc: 'dlc2',
     tags: [],
-    description: 'NO DESCRIPTION YET', // TODO
+    description: `Invoke Lydusa to infuse Ranged and Melee Damage with the power to apply BRITTLE, allowing the Invoker to generate Sand Devils through damage or kills. Lasts 19.5s.
+    Reactivating the skill consumes all Sand Devils generated and casts a 15m Sand Blast dealing 100 Elemental Damage per charge. Max 10 charges.
+    BRITTLE: Increases incoming Critical Chance by 5% and Critical Damage by 25%.`,
+    cooldown: 112.8,
     wikiLinks: [],
     linkedItems: {
       archetype: {
