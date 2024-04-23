65571b32d3c5bc55e85ddaab901372f9266276ce
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-04-23 19:27:56 -0400

    Update add-items.ts

--- src/scripts/add-items.ts
+++ src/scripts/add-items.ts
@@ -3,23 +3,23 @@ import { Item } from '../features/items/types'
 
 const items = [] as const satisfies Item[]
 
-async function main() {
-  for (const item of items) {
-    const dlc = item.dlc ?? 'base'
+// async function main() {
+//   for (const item of items) {
+//     const dlc = item.dlc ?? 'base'
 
-    await prisma.item.upsert({
-      where: { itemId: item.id },
-      update: { dlc }, // update fields if necessary
-      create: { itemId: item.id, dlc },
-    })
-  }
-}
+//     await prisma.item.upsert({
+//       where: { itemId: item.id },
+//       update: { dlc }, // update fields if necessary
+//       create: { itemId: item.id, dlc },
+//     })
+//   }
+// }
 
-main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    console.info('Done with populating items table')
-    await prisma.$disconnect()
-  })
+// main()
+//   .catch((e) => {
+//     throw e
+//   })
+//   .finally(async () => {
+//     console.info('Done with populating items table')
+//     await prisma.$disconnect()
+//   })
