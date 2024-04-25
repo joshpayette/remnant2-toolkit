26ce3bf021fcb811b7f378c047d6190295f0d9bf
Aleksander Lis <158191760+AlexijSeptimus@users.noreply.github.com>
2024-04-25 14:12:16 +0100

    fixed blasting cap ring to match in-game

--- src/app/(data)/items/ringItems.ts
+++ src/app/(data)/items/ringItems.ts
@@ -201,7 +201,7 @@ export const ringItems: RingItem[] = [
     name: 'Blasting Cap Ring',
     imagePath: '/ring/blasting_cap_ring.png',
     id: 'ep14lr',
-    description: `Increases Explosive damage [E] by 15%.`,
+    description: `Increases Explosive damage [E] by 10%.`,
     wikiLinks: [`https://remnant.wiki/Blasting_Cap_Ring`],
   },
   {
