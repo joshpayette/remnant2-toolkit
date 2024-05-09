6a53fc949fb9ef50624c1275fc4a0cf9e891e0e7
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-05-09 19:54:16 -0400

    Update navigation.ts

--- src/app/(types)/navigation.ts
+++ src/app/(types)/navigation.ts
@@ -28,7 +28,7 @@ export const NAV_ITEMS = {
   beginnerBuilds: {
     label: 'Beginner Builds',
     description:
-      'A curated list of builds with items intended to be viewed as a checklist for farming in the earlier hours of gameplay.',
+      'A curated list of builds, intended as an item checklist for farming in the earlier hours of gameplay.',
     href: '/beginner-builds',
     icon: SparklesIcon,
   },
