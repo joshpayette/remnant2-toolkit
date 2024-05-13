5a5226a494564d6430c4cb27960452eef19de866
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-05-12 22:26:05 -0400

    Update world-saves.tsx

--- src/app/world-save-archive/world-saves.tsx
+++ src/app/world-save-archive/world-saves.tsx
@@ -126,7 +126,7 @@ export function WorldSaves({}: Props) {
       )}
 
       {areFiltersApplied ? (
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
           {filteredSaves.map((save) => (
             <WorldSaveCard
               key={`${save.bossName}-${save.bossAffixes.join(',')}`}
