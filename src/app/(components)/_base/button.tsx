4e79da4915a3b6b65e8e600c552d66c92c5ac1b1
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-05-28 20:36:52 -0400

    Updated base button component

--- src/app/(components)/_base/button.tsx
+++ src/app/(components)/_base/button.tsx
@@ -1,7 +1,4 @@
-import {
-  Button as HeadlessButton,
-  type ButtonProps as HeadlessButtonProps,
-} from '@headlessui/react'
+import * as Headless from '@headlessui/react'
 import { clsx } from 'clsx'
 import React from 'react'
 
@@ -11,78 +8,84 @@ const styles = {
   base: [
     // Base
     'relative isolate inline-flex items-center justify-center gap-x-2 rounded-lg border text-base/6 font-semibold',
-
     // Sizing
     'px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] sm:text-sm/6',
-
     // Focus
     'focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-blue-500',
-
     // Disabled
     'data-[disabled]:opacity-50',
-
     // Icon
     '[&>[data-slot=icon]]:-mx-0.5 [&>[data-slot=icon]]:my-0.5 [&>[data-slot=icon]]:size-5 [&>[data-slot=icon]]:shrink-0 [&>[data-slot=icon]]:text-[--btn-icon] [&>[data-slot=icon]]:sm:my-1 [&>[data-slot=icon]]:sm:size-4 forced-colors:[--btn-icon:ButtonText] forced-colors:data-[hover]:[--btn-icon:ButtonText]',
   ],
   solid: [
     // Optical border, implemented as the button background to avoid corner artifacts
-    'border-transparent bg-[--btn-bg]',
-
-    // Subtle white outline is applied using a border
-    'border-surface-solid/5',
-
+    'border-transparent bg-[--btn-border]',
+    // Dark mode: border is rendered on `after` so background is set to button background
+    'dark:bg-[--btn-bg]',
+    // Button background, implemented as foreground layer to stack on top of pseudo-border layer
+    'before:absolute before:inset-0 before:-z-10 before:rounded-[calc(theme(borderRadius.lg)-1px)] before:bg-[--btn-bg]',
+    // Drop shadow, applied to the inset `before` layer so it blends with the border
+    'before:shadow',
+    // Background color is moved to control and shadow is removed in dark mode so hide `before` pseudo
+    'dark:before:hidden',
+    // Dark mode: Subtle white outline is applied using a border
+    'dark:border-white/5',
     // Shim/overlay, inset to match button foreground and used for hover state + highlight shadow
     'after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.lg)-1px)]',
-
     // Inner highlight shadow
-    'after:shadow-[shadow:inset_0_1px_theme(colors.surface.solid/15%)]',
-
+    'after:shadow-[shadow:inset_0_1px_theme(colors.white/15%)]',
     // White overlay on hover
     'after:data-[active]:bg-[--btn-hover-overlay] after:data-[hover]:bg-[--btn-hover-overlay]',
-
-    // `after` layer expands to cover entire button
-    'after:-inset-px after:rounded-lg',
-
+    // Dark mode: `after` layer expands to cover entire button
+    'dark:after:-inset-px dark:after:rounded-lg',
     // Disabled
     'before:data-[disabled]:shadow-none after:data-[disabled]:shadow-none',
   ],
   outline: [
     // Base
-    'border-surface-solid/15 text-surface-solid [--btn-bg:transparent] data-[active]:bg-surface-solid/5 data-[hover]:bg-surface-solid/5',
-
+    'border-zinc-950/10 text-zinc-950 data-[active]:bg-zinc-950/[2.5%] data-[hover]:bg-zinc-950/[2.5%]',
+    // Dark mode
+    'dark:border-white/15 dark:text-white dark:[--btn-bg:transparent] dark:data-[active]:bg-white/5 dark:data-[hover]:bg-white/5',
     // Icon
-    '[--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
+    '[--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.700)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
   ],
   plain: [
     // Base
-    'border-transparent text-surface-solid data-[active]:bg-surface-solid/10 data-[hover]:bg-surface-solid/10',
-
+    'border-transparent text-zinc-950 data-[active]:bg-zinc-950/5 data-[hover]:bg-zinc-950/5',
+    // Dark mode
+    'dark:text-white dark:data-[active]:bg-white/10 dark:data-[hover]:bg-white/10',
     // Icon
-    '[--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.400)] data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
+    '[--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.700)] dark:[--btn-icon:theme(colors.zinc.500)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
   ],
   colors: {
     'dark/zinc': [
-      'text-surface-solid [--btn-bg:theme(colors.zinc.600)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.surface.solid/5%)]',
+      'text-white [--btn-bg:theme(colors.zinc.900)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.surface.solid/10%)]',
+      'dark:text-white dark:[--btn-bg:theme(colors.zinc.600)] dark:[--btn-hover-overlay:theme(colors.surface.solid/5%)]',
       '[--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.300)] data-[hover]:[--btn-icon:theme(colors.zinc.300)]',
     ],
     light: [
-      'text-surface-solid [--btn-bg:theme(colors.zinc.800)] [--btn-border:theme(colors.zinc.950/10%)] [--btn-hover-overlay:theme(colors.surface.solid/5%)] data-[active]:[--btn-border:theme(colors.zinc.950/15%)] data-[hover]:[--btn-border:theme(colors.zinc.950/15%)]',
-      '[--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.400)] data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
+      'text-zinc-950 [--btn-bg:white] [--btn-border:theme(colors.zinc.950/10%)] [--btn-hover-overlay:theme(colors.zinc.950/2.5%)] data-[active]:[--btn-border:theme(colors.zinc.950/15%)] data-[hover]:[--btn-border:theme(colors.zinc.950/15%)]',
+      'dark:text-white dark:[--btn-hover-overlay:theme(colors.surface.solid/5%)] dark:[--btn-bg:theme(colors.zinc.800)]',
+      '[--btn-icon:theme(colors.zinc.500)] data-[active]:[--btn-icon:theme(colors.zinc.700)] data-[hover]:[--btn-icon:theme(colors.zinc.700)] dark:[--btn-icon:theme(colors.zinc.500)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
     ],
     'dark/white': [
-      'text-zinc-950 [--btn-bg:theme(colors.surface.solid)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.zinc.950/5%)]',
-      '[--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.400)] data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
+      'text-white [--btn-bg:theme(colors.zinc.900)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.surface.solid/10%)]',
+      'dark:text-zinc-950 dark:[--btn-bg:white] dark:[--btn-hover-overlay:theme(colors.zinc.950/5%)]',
+      '[--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.300)] data-[hover]:[--btn-icon:theme(colors.zinc.300)] dark:[--btn-icon:theme(colors.zinc.500)] dark:data-[active]:[--btn-icon:theme(colors.zinc.400)] dark:data-[hover]:[--btn-icon:theme(colors.zinc.400)]',
     ],
     dark: [
-      'text-surface-solid [--btn-bg:theme(colors.zinc.800)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.surface.solid/5%)]',
+      'text-white [--btn-bg:theme(colors.zinc.900)] [--btn-border:theme(colors.zinc.950/90%)] [--btn-hover-overlay:theme(colors.surface.solid/10%)]',
+      'dark:[--btn-hover-overlay:theme(colors.surface.solid/5%)] dark:[--btn-bg:theme(colors.zinc.800)]',
       '[--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.300)] data-[hover]:[--btn-icon:theme(colors.zinc.300)]',
     ],
     white: [
-      'text-zinc-950 [--btn-bg:theme(colors.surface.solid)] [--btn-border:theme(colors.zinc.950/10%)] [--btn-hover-overlay:theme(colors.zinc.950/5%)] data-[active]:[--btn-border:theme(colors.zinc.950/15%)] data-[hover]:[--btn-border:theme(colors.zinc.950/15%)]',
+      'text-zinc-950 [--btn-bg:white] [--btn-border:theme(colors.zinc.950/10%)] [--btn-hover-overlay:theme(colors.zinc.950/2.5%)] data-[active]:[--btn-border:theme(colors.zinc.950/15%)] data-[hover]:[--btn-border:theme(colors.zinc.950/15%)]',
+      'dark:[--btn-hover-overlay:theme(colors.zinc.950/5%)]',
       '[--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.500)] data-[hover]:[--btn-icon:theme(colors.zinc.500)]',
     ],
     zinc: [
-      'text-white [--btn-hover-overlay:theme(colors.surface.solid/5%)] [--btn-bg:theme(colors.zinc.600)] [--btn-border:theme(colors.zinc.700/90%)]',
+      'text-white [--btn-hover-overlay:theme(colors.surface.solid/10%)] [--btn-bg:theme(colors.zinc.600)] [--btn-border:theme(colors.zinc.700/90%)]',
+      'dark:[--btn-hover-overlay:theme(colors.surface.solid/5%)]',
       '[--btn-icon:theme(colors.zinc.400)] data-[active]:[--btn-icon:theme(colors.zinc.300)] data-[hover]:[--btn-icon:theme(colors.zinc.300)]',
     ],
     indigo: [
@@ -160,9 +163,9 @@ type ButtonProps = (
   | { color?: keyof typeof styles.colors; outline?: never; plain?: never }
   | { color?: never; outline: true; plain?: never }
   | { color?: never; outline?: never; plain: true }
-) & { children: React.ReactNode } & (
-    | HeadlessButtonProps
-    | React.ComponentPropsWithoutRef<typeof Link>
+) & { className?: string; children: React.ReactNode } & (
+    | Omit<Headless.ButtonProps, 'className'>
+    | Omit<React.ComponentPropsWithoutRef<typeof Link>, 'className'>
   )
 
 export const BaseButton = React.forwardRef(function Button(
@@ -188,25 +191,27 @@ export const BaseButton = React.forwardRef(function Button(
       <TouchTarget>{children}</TouchTarget>
     </Link>
   ) : (
-    <HeadlessButton
+    <Headless.Button
       {...props}
       className={clsx(classes, 'cursor-default')}
       ref={ref}
     >
       <TouchTarget>{children}</TouchTarget>
-    </HeadlessButton>
+    </Headless.Button>
   )
 })
 
-/* Expand the hit area to at least 44×44px on touch devices */
+/**
+ * Expand the hit area to at least 44×44px on touch devices
+ */
 export function TouchTarget({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {children}
       <span
         className="absolute left-1/2 top-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 [@media(pointer:fine)]:hidden"
         aria-hidden="true"
       />
+      {children}
     </>
   )
 }
