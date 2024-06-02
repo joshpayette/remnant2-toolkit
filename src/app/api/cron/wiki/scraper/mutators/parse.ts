652c48a775c288ebb40dc5f8ebaeb5eb9626b661
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-06-02 08:43:03 -0400

    Updated mutator parsing to handle mutators with separate bow descriptions

--- src/app/api/cron/wiki/scraper/mutators/parse.ts
+++ src/app/api/cron/wiki/scraper/mutators/parse.ts
@@ -2,6 +2,8 @@ import { CheerioAPI } from 'cheerio'
 
 import { MutatorItem } from '@/app/(data)/items/types/MutatorItem'
 
+const mutatorsWithBowDescriptions = ['Bandit', 'Extender', 'Transpose']
+
 export function mutatorDataParse(
   $: CheerioAPI,
   item: MutatorItem,
@@ -9,28 +11,66 @@ export function mutatorDataParse(
   description: string
   maxLevelBonus: string
 } {
-  const mutatorsWithTabberPanels = ['Bandit']
-
-  const wholeDescription = mutatorsWithTabberPanels.some(
+  const mutatorHasBowDescription = mutatorsWithBowDescriptions.some(
     (i) => i.toLowerCase() === item.name.toLowerCase(),
   )
-    ? $('div.infobox-description article.tabber__panel')
-        .find('br')
-        .replaceWith('\n')
-        .end()
-        .text()
-        .replaceAll('[sic]', '')
-    : $('div.infobox-description')
-        .find('br')
-        .replaceWith('\n')
-        .end()
-        .text()
-        .replaceAll('[sic]', '')
+
+  if (!mutatorHasBowDescription) {
+    const wholeDescription = $('div.infobox-description')
+      .find('br')
+      .replaceWith('\n')
+      .end()
+      .text()
+      .replaceAll('[sic]', '')
+
+    // The description is everything before the two line breaks and the text Level 10
+    const description = wholeDescription.split('Level 10:')[0].trim()
+    // The maxLevelBonus is everything starting with the Level 10 text
+    const maxLevelBonus = wholeDescription.split('Level 10:')[1].trim()
+
+    return {
+      description,
+      maxLevelBonus,
+    }
+  }
+
+  const standardWholeDescription = $(
+    'div.infobox-description [data-title="Standard"]',
+  )
+    .find('br')
+    .replaceWith('\n')
+    .end()
+    .text()
+    .replaceAll('[sic]', '')
+
+  const bowWholeDescription = $('div.infobox-description [data-title="Bow"]')
+    .find('br')
+    .replaceWith('\n')
+    .end()
+    .text()
+    .replaceAll('[sic]', '')
 
   // The description is everything before the two line breaks and the text Level 10
-  const description = wholeDescription.split('Level 10:')[0].trim()
+  const standardDescription = standardWholeDescription
+    .split('Level 10:')[0]
+    .trim()
   // The maxLevelBonus is everything starting with the Level 10 text
-  const maxLevelBonus = wholeDescription.split('Level 10:')[1].trim()
+  const standardMaxLevelBonus = standardWholeDescription
+    .split('Level 10:')[1]
+    .trim()
+
+  const bowDescription = bowWholeDescription.split('Level 10:')[0].trim()
+  const bowMaxLevelBonus = bowWholeDescription.split('Level 10:')[1].trim()
+
+  const description =
+    standardDescription === bowDescription
+      ? standardDescription
+      : `${standardDescription}\n\nBows: ${bowDescription}`
+
+  const maxLevelBonus =
+    standardMaxLevelBonus === bowMaxLevelBonus
+      ? standardMaxLevelBonus
+      : `${standardMaxLevelBonus}\n\nBows: ${bowMaxLevelBonus}`
 
   return {
     description,
