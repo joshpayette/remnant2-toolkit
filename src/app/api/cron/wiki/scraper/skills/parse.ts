0764fecdf218e99ed62bdbc3deb86e7935eb62bd
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-06-02 08:53:13 -0400

    Updated skill item wiki parser

--- src/app/api/cron/wiki/scraper/skills/parse.ts
+++ src/app/api/cron/wiki/scraper/skills/parse.ts
@@ -1,40 +1,24 @@
 import { CheerioAPI } from 'cheerio'
 
+// TODO Check a hunter skill
+
 export function skillDataParse($: CheerioAPI): {
   description: string
   cooldown: number
 } {
-  const wholeDescription = $('div.infobox-description')
+  const descriptionEl = $('.portable-infobox [data-source="description"] div')
+
+  const description = descriptionEl
     .find('br')
     .replaceWith('\n')
     .end()
     .text()
     .replaceAll('[sic]', '')
+    .replaceAll('\t', '')
 
   // Not every skill has a cooldown
-  const hasCooldown = wholeDescription.includes('Cooldown:')
-
-  // Hunter skills have the MARK text after the cooldown
-  const hasMark = wholeDescription.includes('MARK:')
-
-  if (!hasCooldown) {
-    return {
-      description: wholeDescription.trim(),
-      cooldown: 0,
-    }
-  }
-
-  // The description is everything before the two line breaks and the text Cooldown:
-  let description = wholeDescription.split('Cooldown:')[0].trim()
-  // The cooldown is everything starting with the Cooldown: text
-  let cooldown = wholeDescription.split('Cooldown:')[1].trim()
-
-  if (hasMark) {
-    // Need to remove the MARK text from the cooldown, then append it to the description
-    const mark = cooldown.split('MARK:')[1].trim()
-    cooldown = cooldown.split('MARK:')[0].trim()
-    description += `\n\nMARK: ${mark}`
-  }
+  const cooldownEl = $('.portable-infobox [data-source="cooldown"] div')
+  const cooldown = cooldownEl.length ? cooldownEl.text() : '0'
 
   return {
     description,
