4f531a502aa835b0a6dfc8e108e66c6470081386
Matthew <matthew.alexander.hawkins@gmail.com>
2024-06-02 17:38:30 -0700

    Launch dark/light mode toggle (remove feature gate)

--- src/app/(components)/buttons/global-action-buttons/theme-toggle-button.client.tsx
+++ src/app/(components)/buttons/global-action-buttons/theme-toggle-button.client.tsx
@@ -6,14 +6,7 @@ import { BaseButton } from '@/app/(components)/_base/button'
 // WARNING: This component is not hydration-safe
 // https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
 export default function ToggleThemeButton() {
-  const { setTheme, forcedTheme, resolvedTheme } = useTheme()
-
-  // Temporary feature gate
-  if (forcedTheme) {
-    // If a theme is being forced, there is no alternate theme to toggle to
-    // Disable the toggle option in this case
-    return
-  }
+  const { setTheme, resolvedTheme } = useTheme()
 
   const isDark = resolvedTheme === 'dark'
   const ButtonIcon = isDark ? SunIcon : MoonIcon
--- src/app/(components)/theme-selection.client.tsx
+++ src/app/(components)/theme-selection.client.tsx
@@ -1,19 +1,13 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
-import { useLocalStorage } from 'usehooks-ts'
 
 export default function ThemeSelection({ children }: { children: React.ReactNode }) {
-  const [localValue] = useLocalStorage<boolean>('r2tk-theme-toggle', false)
   return (
     <ThemeProvider
       attribute="class"
       defaultTheme="system"
       enableSystem
-      // This is a feature gate
-      // If you wish to opt in, you must manually add this to your localstorage
-      // Note that you will need to refresh the page afterwards
-      forcedTheme={localValue ? undefined : 'dark'}
     >
       {children}
     </ThemeProvider>
