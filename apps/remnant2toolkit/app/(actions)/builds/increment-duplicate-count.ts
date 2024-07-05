0176a301e9bb59b1e44d19a61541d1301c4c1444
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-07-05 15:51:04 -0400

    Added logic to prevent increments for builds created by the user

--- apps/remnant2toolkit/app/(actions)/builds/increment-duplicate-count.ts
+++ apps/remnant2toolkit/app/(actions)/builds/increment-duplicate-count.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from '@repo/db'
 
+import { getServerSession } from '@/app/(features)/auth'
 import type { BuildActionResponse } from '@/app/(types)/builds'
 
 export async function incrementDuplicateCount({
@@ -9,7 +10,29 @@ export async function incrementDuplicateCount({
 }: {
   buildId: string
 }): Promise<BuildActionResponse> {
+  const session = await getServerSession()
+  const userId = session?.user?.id
+
   try {
+    // If the build is created by the user, do not add a count
+    if (userId) {
+      const build = await prisma.build.findUnique({
+        where: {
+          id: buildId,
+        },
+        select: {
+          createdById: true,
+        },
+      })
+
+      if (build?.createdById === userId) {
+        return {
+          message:
+            'Duplicate count not incremented as the build is created by the user!',
+        }
+      }
+    }
+
     await prisma.build.update({
       where: {
         id: buildId,
--- apps/remnant2toolkit/app/(actions)/builds/increment-view-count.ts
+++ apps/remnant2toolkit/app/(actions)/builds/increment-view-count.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from '@repo/db'
 
+import { getServerSession } from '@/app/(features)/auth'
 import type { BuildActionResponse } from '@/app/(types)/builds'
 
 export async function incrementViewCount({
@@ -9,7 +10,29 @@ export async function incrementViewCount({
 }: {
   buildId: string
 }): Promise<BuildActionResponse> {
+  const session = await getServerSession()
+  const userId = session?.user?.id
+
   try {
+    // If the build is created by the user, do not add a view
+    if (userId) {
+      const build = await prisma.build.findUnique({
+        where: {
+          id: buildId,
+        },
+        select: {
+          createdById: true,
+        },
+      })
+
+      if (build?.createdById === userId) {
+        return {
+          message:
+            'View count not incremented as the build is created by the user!',
+        }
+      }
+    }
+
     await prisma.build.update({
       where: {
         id: buildId,
