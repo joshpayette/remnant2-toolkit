5e957ae49d698a107427db7aebf8775db28e69ca
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-07-05 20:51:05 -0400

    Added a bit more space for new builds badge

--- apps/remnant2toolkit/app/quality-builds-feed.tsx
+++ apps/remnant2toolkit/app/quality-builds-feed.tsx
@@ -35,10 +35,10 @@ export function QualityBuildsFeed({ builds }: Props) {
       </div>
       <ul
         role="list"
-        className="mb-2 mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+        className="my-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
       >
         {builds.map((build) => (
-          <div key={build.id} className="w-full">
+          <div key={build.id} className="mt-4 w-full">
             <BuildCard
               build={build}
               isLoading={false}
