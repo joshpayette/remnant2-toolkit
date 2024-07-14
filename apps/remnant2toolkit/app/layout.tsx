bf3985034384d52dc63dbbd86c1685668c2cb65b
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-07-14 16:46:33 -0400

    Squashed commit of the following:
    commit 714fe843399803b3f76d953b03c1bda8408c8180
    Author: Josh Payette <139721968+joshpayette@users.noreply.github.com>
    Date:   Sun Jul 14 16:45:42 2024 -0400
    
        Restructured layout folder
    
    commit eaceed4bf320e5fb95d2dc031e39eb1b7cabe264
    Author: Josh Payette <139721968+joshpayette@users.noreply.github.com>
    Date:   Sun Jul 14 16:43:22 2024 -0400
    
        split shared layout into shared component

--- apps/remnant2toolkit/app/layout.tsx
+++ apps/remnant2toolkit/app/layout.tsx
@@ -1,35 +1,23 @@
-import { cn } from '@repo/ui/classnames'
-import { Inter } from 'next/font/google'
-export { metadata } from './metadata'
 import './globals.css'
-import '@repo/ui/styles.css'
-import 'react-toastify/dist/ReactToastify.min.css'
 
 import { GlobalActionButtons } from '@repo/ui/global-action-buttons'
-import { PreloadResources } from '@repo/ui/preload-resources'
-import { SessionProvider } from '@repo/ui/session-provider'
+import { RootLayout } from '@repo/ui/pages/root-layout'
 import { Analytics } from '@vercel/analytics/react'
 import { Viewport } from 'next'
 import dynamic from 'next/dynamic'
-import { ToastContainer } from 'react-toastify'
 
 import { Footer } from '@/app/(components)/footer'
 import { Navbar } from '@/app/(components)/navbar'
 import { getServerSession } from '@/app/(features)/auth'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export const viewport: Viewport = {}
+export { metadata } from './metadata'
 
 const AlertBanner = dynamic(() => import('@repo/ui/alert-banner'), {
   ssr: false,
 })
 
-const ThemeSelection = dynamic(() => import('@repo/ui/theme/theme-selection'), {
-  ssr: false,
-})
-
-export default async function RootLayout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode
@@ -37,41 +25,19 @@ export default async function RootLayout({
   const session = await getServerSession()
 
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          'relative flex min-h-fit flex-col items-center justify-start',
-          inter.className,
-        )}
-      >
-        <PreloadResources />
-        <SessionProvider>
-          <ThemeSelection>
-            <GlobalActionButtons
-              username={session?.user?.name || 'Unknown User'}
-            />
-            <AlertBanner localStorageKey="view-count-alert">
-              Builds now track views and validated views, and can be filtered by
-              validated views. Try it out!
-            </AlertBanner>
-            <div className="flex h-full w-full max-w-7xl grow flex-col items-start justify-start">
-              <header className="w-full">
-                <Navbar />
-              </header>
-
-              <main className="mt-[80px] flex h-full min-h-screen w-full grow flex-col items-center justify-start p-4 pt-0">
-                <ToastContainer theme="dark" pauseOnFocusLoss={false} />
-                {children}
-              </main>
-            </div>
-
-            <Footer />
-          </ThemeSelection>
-        </SessionProvider>
-        <Analytics />
-        {/* <SpeedInsights /> */}
-      </body>
-    </html>
+    <RootLayout
+      alertBanner={
+        <AlertBanner localStorageKey="view-count-alert">
+          Builds now track views and validated views, and can be filtered by
+          validated views. Try it out!
+        </AlertBanner>
+      }
+      footer={<Footer />}
+      navbar={<Navbar />}
+      trackers={<Analytics />}
+    >
+      <GlobalActionButtons username={session?.user?.name || 'Unknown User'} />
+      {children}
+    </RootLayout>
   )
 }
