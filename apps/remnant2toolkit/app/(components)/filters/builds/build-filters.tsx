e829120ed7d8e7445fa13c23b51529365218d1a1
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-07-18 21:47:01 -0400

    Update build-filters.tsx

--- apps/remnant2toolkit/app/(components)/filters/builds/build-filters.tsx
+++ apps/remnant2toolkit/app/(components)/filters/builds/build-filters.tsx
@@ -504,14 +504,15 @@ export function BuildFilters({ buildFiltersOverrides, loadingResults }: Props) {
                       ]}
                       onChange={handleMiscFilterChange}
                     />
-                    <div className="flex items-center justify-end">
+                    {/** TODO Quality Builds */}
+                    {/* <div className="flex items-center justify-end">
                       <BaseButton
                         plain
                         onClick={() => setQualityBuildDialogOpen(true)}
                       >
                         What makes a Quality Build?
                       </BaseButton>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
                 <div className="flex w-full items-center justify-end gap-x-4">
