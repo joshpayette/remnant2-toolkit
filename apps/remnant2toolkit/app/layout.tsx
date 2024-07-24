81c774486439cee7903e33983b2d9e71f5003e25
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-07-23 23:33:04 -0400

    Update layout.tsx

--- apps/remnant2toolkit/app/layout.tsx
+++ apps/remnant2toolkit/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function Layout({
         <AlertBanner localStorageKey="base-game-builds">
           We just added a{' '}
           <BaseTextLink href="/base-game-builds">Base Game Builds</BaseTextLink>{' '}
-          page for users without any DLC! Check it out.
+          page for users without any DLC. Check it out!
         </AlertBanner>
       }
       footer={<Footer />}
