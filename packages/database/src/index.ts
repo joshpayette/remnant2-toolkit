9c734eb082737b2430c87aab4462a03fddaf6f4f
Josh Payette <139721968+joshpayette@users.noreply.github.com>
2024-08-05 22:03:05 -0400

    Updated local configuration

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -8,7 +8,10 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['error', 'warn'] // 'query' to include query logging
+        : ['error'],
   })
 
 if (process.env.NODE_ENV !== 'production') {
