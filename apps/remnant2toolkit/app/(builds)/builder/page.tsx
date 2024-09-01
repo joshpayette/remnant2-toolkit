60213a65ed983cfb8e3ca63e0b847a08128cfa1b
Josh Payette <hello@joshpayette.dev>
2024-09-01 15:34:49 -0400

    Refactored url build state logic
    Removed the hook and instead inlined it to the component. It was only being used in one place anyway.

--- apps/remnant2toolkit/app/(builds)/builder/page.tsx
+++ apps/remnant2toolkit/app/(builds)/builder/page.tsx
@@ -1,66 +1,50 @@
-'use client';
-
-import { BaseLink, Skeleton } from '@repo/ui';
-import { useSession } from 'next-auth/react';
-import { useRef, useState } from 'react';
+import { BaseLink } from '@repo/ui';
+import { type Metadata } from 'next';
 
 import { PageHeader } from '@/app/_components/page-header';
-import { ToCsvButton } from '@/app/_components/to-csv-button';
-import { LongUrlAlert } from '@/app/(builds)/_components/long-url-alert';
-import { useBuildActions } from '@/app/(builds)/_hooks/use-build-actions';
-import { useUrlBuildState } from '@/app/(builds)/_utils/hooks/use-url-build-state';
-import { BuilderContainer } from '@/app/(builds)/builder/_components/builder-container';
-import { DetailedViewButton } from '@/app/(builds)/builder/_components/buttons/detailed-view-button';
-import { GenerateBuildImageButton } from '@/app/(builds)/builder/_components/buttons/generate-build-image';
-import { SaveBuildButton } from '@/app/(builds)/builder/_components/buttons/save-build-button';
-import { ShareBuildButton } from '@/app/(builds)/builder/_components/buttons/share-build-button';
-import { DetailedBuildDialog } from '@/app/(builds)/builder/_components/dialogs/detailed-build-dialog';
-import { ImageDownloadInfoDialog } from '@/app/(builds)/builder/_components/dialogs/image-download-info-dialog';
+import { NAV_ITEMS } from '@/app/_types/navigation';
+import { CreateUrlBuild } from '@/app/(builds)/builder/create-url-build';
+import { getSession } from '@/app/(user)/_auth/services/sessionService';
 
-export default function Page() {
-  const [detailedBuildDialogOpen, setDetailedBuildDialogOpen] = useState(false);
-  const [shareBuildAlertOpen, setShareBuildAlertOpen] = useState(false);
+export async function generateMetadata(): Promise<Metadata> {
+  const title = `Build Creation Tool - Remnant 2 Toolkit`;
+  const description = NAV_ITEMS.createBuild.description;
 
-  const { data: session, status: sessionStatus } = useSession();
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description: description,
+      siteName: 'Remnant 2 Toolkit',
+      url: `https://remnant2toolkit.com/builder/create`,
+      images: [
+        {
+          url: 'https://d2sqltdcj8czo5.cloudfront.net/remnant2/misc/og-image-sm.jpg',
+          width: 150,
+          height: 150,
+        },
+      ],
+      type: 'website',
+    },
+    twitter: {
+      title,
+      description,
+    },
+  };
+}
 
-  const {
-    csvItems,
-    urlBuildState: buildState,
-    updateUrlBuildState,
-  } = useUrlBuildState();
-
-  const {
-    isScreenshotMode,
-    showControls,
-    imageExportLoading,
-    imageDownloadInfo,
-    handleClearImageDownloadInfo,
-    handleImageExport,
-  } = useBuildActions();
-
-  const buildContainerRef = useRef<HTMLDivElement>(null);
+export default async function Page() {
+  const session = await getSession();
 
   return (
     <div className="flex w-full flex-col items-center">
-      <DetailedBuildDialog
-        buildState={buildState}
-        open={detailedBuildDialogOpen}
-        onClose={() => setDetailedBuildDialogOpen(false)}
-      />
-
-      <ImageDownloadInfoDialog
-        onClose={handleClearImageDownloadInfo}
-        imageDownloadInfo={imageDownloadInfo}
-      />
-
       <PageHeader
         title="Remnant 2 Build Tool"
         subtitle="Create your builds and share them with your friends and the community."
       />
 
-      {sessionStatus === 'loading' ? (
-        <Skeleton />
-      ) : session?.user ? (
+      {session?.user ? (
         <div className="text-surface-solid my-4 max-w-lg rounded-md border border-red-500 px-2 py-1 text-left">
           <h3 className="text-center text-2xl font-bold">
             Features limited on this page!
@@ -111,57 +95,7 @@ export default function Page() {
           </ul>
         </div>
       )}
-
-      <BuilderContainer
-        buildContainerRef={buildContainerRef}
-        buildState={buildState}
-        isEditable={true}
-        isScreenshotMode={isScreenshotMode}
-        itemOwnershipPreference={false}
-        showControls={showControls}
-        showCreatedBy={false}
-        showMemberFeatures={false}
-        onUpdateBuildState={updateUrlBuildState}
-        builderActions={
-          <>
-            {session?.user && (
-              <SaveBuildButton buildState={buildState} editMode={false} />
-            )}
-
-            <GenerateBuildImageButton
-              imageExportLoading={imageExportLoading}
-              onClick={() =>
-                handleImageExport(
-                  buildContainerRef.current,
-                  `${buildState.name}`,
-                )
-              }
-            />
-
-            {session?.user ? null : (
-              <>
-                <LongUrlAlert
-                  open={shareBuildAlertOpen}
-                  onClose={() => setShareBuildAlertOpen(false)}
-                />
-                <ShareBuildButton
-                  onClick={() => setShareBuildAlertOpen(true)}
-                />
-              </>
-            )}
-
-            <DetailedViewButton
-              onClick={() => setDetailedBuildDialogOpen(true)}
-            />
-
-            <ToCsvButton
-              data={csvItems}
-              filename={`remnant2_builder_${buildState.name}`}
-              label="Export to CSV"
-            />
-          </>
-        }
-      />
+      <CreateUrlBuild />
     </div>
   );
 }
