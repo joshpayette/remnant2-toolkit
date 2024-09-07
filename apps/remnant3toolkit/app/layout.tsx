3cef23bce28894d1256d28f3b690539c8f089963
Josh Payette <hello@joshpayette.dev>
2024-09-06 20:10:54 -0400

    Added placeholder remnant3toolkit

---
+++ apps/remnant3toolkit/app/layout.tsx
@@ -0,0 +1,37 @@
+import './globals.css';
+
+import { type Viewport } from 'next';
+import { Inter } from 'next/font/google';
+
+export const viewport: Viewport = {};
+export { metadata } from './metadata';
+import { cn, PreloadResources } from '@repo/ui';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          'relative flex min-h-fit flex-col items-center justify-start bg-gradient-to-br from-purple-500 to-purple-700 text-white',
+          inter.className,
+        )}
+      >
+        <PreloadResources />
+        <div className="flex h-full w-full max-w-[1500px] grow flex-col items-start justify-start">
+          <header className="w-full"></header>
+
+          <main className="mt-[80px] flex h-full min-h-screen w-full grow flex-col items-center justify-start p-4 pt-0">
+            {children}
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ apps/remnant3toolkit/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="text-6xl">Remnant 3 Toolkit...soon™ ???</h1>
+    </div>
+  );
+}
