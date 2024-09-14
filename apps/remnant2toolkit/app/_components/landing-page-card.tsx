0875dea2756d53b14b56ad61fed81d4c8a55d48a
Josh Payette <hello@joshpayette.dev>
2024-09-14 08:37:59 -0400

    Reverted wiki icon update

--- apps/remnant2toolkit/app/_components/landing-page-card.tsx
+++ apps/remnant2toolkit/app/_components/landing-page-card.tsx
@@ -1,7 +1,6 @@
 import { ArrowRightIcon, BaseLink, cn } from '@repo/ui';
 
 interface Props {
-  variant?: 'wiki';
   href: string;
   target?: '_blank';
   label: string;
@@ -10,7 +9,6 @@ interface Props {
 }
 
 export function LandingPageCard({
-  variant,
   href,
   target,
   description,
@@ -24,18 +22,15 @@ export function LandingPageCard({
       target={target}
       className={cn(
         'bg-background-solid/50 ring-background-solid/10 hover:border-primary-500 mb-4 flex min-h-[200px] flex-row gap-x-4 rounded-xl border border-transparent p-6 ring-1 ring-inset',
-        variant === 'wiki' &&
-          "bg-[url('https://d2sqltdcj8czo5.cloudfront.net/remnant2/misc/remwiki_watermark.png')] bg-contain bg-center bg-no-repeat",
       )}
     >
       {icon}
       <div className="relative w-full">
-        {variant !== 'wiki' && (
-          <>
-            <p className="text-lg font-bold leading-7">{label}</p>
-            <div className="mt-2 text-gray-300 ">{description}</div>
-          </>
-        )}
+        <>
+          <p className="text-lg font-bold leading-7">{label}</p>
+          <div className="mt-2 text-gray-300 ">{description}</div>
+        </>
+
         <div className="absolute bottom-0 right-0 mt-4 flex w-full items-center justify-end">
           <ArrowRightIcon
             className="text-primary-500 hover:text-primary-300 h-6 w-6"
