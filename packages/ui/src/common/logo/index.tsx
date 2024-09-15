f21afbbb9764a8eb14b74358eee3a6b8458af268
Josh Payette <hello@joshpayette.dev>
2024-09-15 16:11:54 -0400

    Font fix?

--- packages/ui/src/common/logo/index.tsx
+++ packages/ui/src/common/logo/index.tsx
@@ -8,10 +8,16 @@ const variants = {
     imageUrl: '/remnant2/misc/toolkit-logo.png',
     alt: 'Remnant 2 Toolkit logo, a purple and yellow toolbox.',
     text: (
-      <div className="ui-text-[16px] ui-leading-4 ui-text-surface-solid ui-mb-0 ui-pb-0 ui-font-black ui-flex ui-gap-x-0.5">
-        <div>REMNANT</div>
-        <div className="ui-text-red-500">II</div>
-        <div>TOOLKIT</div>
+      <div className="ui-mb-0 ui-pb-0 ui-flex ui-gap-x-0.5">
+        <p className="ui-font-black ui-text-[16px] ui-leading-4 ui-antialiased ui-text-surface-solid">
+          REMNANT
+        </p>
+        <p className="ui-font-black ui-leading-4 ui-text-red-500 ui-text-[16px] ui-antialiased">
+          II
+        </p>
+        <p className="ui-font-black ui-leading-4 ui-text-[16px] ui-antialiased ui-text-surface-solid">
+          TOOLKIT
+        </p>
       </div>
     ),
     subtext: 'Powered by Remnant.wiki + Cowaii.io',
@@ -48,13 +54,14 @@ export function Logo({
         height={36}
         loading="eager"
         priority
+        quality={90}
         src={`https://d2sqltdcj8czo5.cloudfront.net${variants[variant].imageUrl}`}
         unoptimized={unoptimized}
         width={52}
       />
       <div className="ui-flex ui-flex-col ui-gap-0">
         {variants[variant].text}
-        <span className="ui-text-[10px] ui-font-semibold ui-leading-3 ui-text-surface-solid ui-mb-0 ui-pb-0">
+        <span className="ui-text-[10px] ui-font-bold ui-antialiased ui-leading-3 ui-text-surface-solid ui-mb-0 ui-pb-0">
           {variants[variant].subtext}
         </span>
         {showUrl ? (
