cefa0d1c756c63ed57148da137493a03bb3e9a94
Josh Payette <hello@joshpayette.dev>
2024-09-24 16:13:47 -0400

    .

--- _DLC STUFF/add-items.sql
+++ _DLC STUFF/add-items.sql
@@ -1,8 +1,10 @@
 INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
 
-INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dim4rk0", "im4rk0", "dlc3", 1);
-INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7d1asxjo", "1asxjo", "dlc3", 1);
-INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7d542njq", "542njq", "dlc3", 1);
+INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
+INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
+INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
+INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
+INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
 INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
 INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
 INSERT INTO Item (id, itemId, dlc, isActive) VALUES ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
