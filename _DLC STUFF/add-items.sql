fd2bfe9798f4f22ae3a2c8797eb5d587c85302dd
Josh Payette <hello@joshpayette.dev>
2024-09-26 07:32:39 -0400

    Added item

--- _DLC STUFF/add-items.sql
+++ _DLC STUFF/add-items.sql
@@ -1,17 +1,7 @@
 INSERT INTO
   Item (id, itemId, dlc, isActive)
 VALUES
-  ("clrlgfe0j0000eioq7dokmk07", "okmk07", "dlc3", 1);
-
-INSERT INTO
-  Item (id, itemId, dlc, isActive)
-VALUES
-  ("clrlgfe0j0000eioq7d5s9x12", "5s9x12", "dlc3", 1);
-
-INSERT INTO
-  Item (id, itemId, dlc, isActive)
-VALUES
-  ("clrlgfe0j0000eioq7dXXXXXX", "XXXXXX", "dlc3", 1);
+  ("clrlgfe0j0000eioq7d3kbtu8", "3kbtu8", "dlc3", 1);
 
 INSERT INTO
   Item (id, itemId, dlc, isActive)
