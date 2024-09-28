16fc061e209e124ce739e1f2811f4835e16fb560
Josh Payette <hello@joshpayette.dev>
2024-09-27 20:54:54 -0400

    Updated quality build logic

--- apps/remnant2toolkit/app/(builds)/_constants/max-trait-amount.ts
+++ apps/remnant2toolkit/app/(builds)/_constants/max-trait-amount.ts
@@ -3,3 +3,11 @@
  * be added to the builder.
  */
 export const MAX_TRAIT_AMOUNT = 110;
+
+/**
+ * Traitor legendary fragment grants max core amount.
+ * Each class gets 5 points spread across all 4 core amounts.
+ * This means Traitor invests 35 points to max core amounts.
+ */
+const MAX_CORE_AMOUNT = 35;
+export const MAX_TRAIT_AMOUNT_WITH_TRAITOR = MAX_TRAIT_AMOUNT + MAX_CORE_AMOUNT;
--- apps/remnant2toolkit/app/(builds)/_libs/build-filters/limit-by-quality.ts
+++ apps/remnant2toolkit/app/(builds)/_libs/build-filters/limit-by-quality.ts
@@ -1,7 +1,10 @@
 import { Prisma } from '@repo/db';
 
 import { MINIMUM_QUALITY_DESCRIPTION_LENGTH } from '@/app/(builds)/_components/filters/build-misc-filter';
-import { MAX_TRAIT_AMOUNT } from '@/app/(builds)/_constants/max-trait-amount';
+import {
+  MAX_TRAIT_AMOUNT,
+  MAX_TRAIT_AMOUNT_WITH_TRAITOR,
+} from '@/app/(builds)/_constants/max-trait-amount';
 
 export function limitToQualityBuilds(limitToQualityBuilds: boolean) {
   if (!limitToQualityBuilds) return Prisma.empty;
@@ -18,6 +21,6 @@ export function limitToQualityBuilds(limitToQualityBuilds: boolean) {
   AND (ItemCounts.mutatorCount = 3)
   AND (ItemCounts.amuletCount = 1)
   AND (ItemCounts.ringCount = 4)
-  AND (ItemCounts.traitSum = ${MAX_TRAIT_AMOUNT})
+  AND (ItemCounts.traitSum = ${MAX_TRAIT_AMOUNT} OR ItemCounts.traitSum = ${MAX_TRAIT_AMOUNT_WITH_TRAITOR})
   `;
 }
