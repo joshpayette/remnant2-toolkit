53c32bb7b3513027ee77915806c69d67d95876ea
Josh Payette <hello@joshpayette.dev>
2024-09-29 11:14:27 -0400

    Removed collection information from random-builds

--- apps/remnant2toolkit/app/(builds)/random-build/page.tsx
+++ apps/remnant2toolkit/app/(builds)/random-build/page.tsx
@@ -7,9 +7,6 @@ import { NAV_ITEMS } from '@/app/_types/navigation';
 import { getRandomBuild } from '@/app/(builds)/_libs/get-random-build';
 import { CreateBuild } from '@/app/(builds)/builder/create/create-build';
 import { allItems } from '@/app/(items)/_constants/all-items';
-import { FusionItem } from '@/app/(items)/_types/fusion-item';
-import { type Item } from '@/app/(items)/_types/item';
-import { getDiscoveredItems } from '@/app/(items)/item-tracker/_actions/get-discovered-items';
 import { getSession } from '@/app/(user)/_auth/services/sessionService';
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -42,56 +39,8 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function Page() {
   const session = await getSession();
-  const discoveredItemResponse = session?.user
-    ? await getDiscoveredItems()
-    : {
-        success: false,
-        message: 'You must be logged in to track items to the database.',
-        discoveredItemIds: undefined,
-      };
 
-  const discoveredItemIds = discoveredItemResponse.discoveredItemIds;
-
-  const itemList = discoveredItemIds
-    ? discoveredItemIds.map((itemId) => {
-        return allItems.find((item) => item.id === itemId) as Item;
-      })
-    : undefined;
-
-  // Add discovered skills and perks to the list
-  if (itemList) {
-    const discoveredArchetypes = itemList.filter(
-      (item) => item.category === 'archetype',
-    );
-
-    for (const archetypeItem of discoveredArchetypes) {
-      if (
-        !archetypeItem.linkedItems?.skills ||
-        !archetypeItem.linkedItems?.perks
-      ) {
-        continue;
-      }
-
-      for (const skill of archetypeItem.linkedItems.skills) {
-        const skillItem = allItems.find((i) => i.name === skill.name);
-        if (skillItem) {
-          itemList.push(skillItem);
-        }
-      }
-
-      for (const perk of archetypeItem.linkedItems.perks) {
-        const perkItem = allItems.find((i) => i.name === perk.name);
-        if (perkItem) {
-          itemList.push(perkItem);
-        }
-      }
-    }
-  }
-
-  const randomBuildState =
-    itemList && itemList.length > 0
-      ? getRandomBuild(allItems) // TODO Fix this to work with your collection
-      : getRandomBuild(allItems);
+  const randomBuildState = getRandomBuild(allItems);
 
   return (
     <div className="flex w-full flex-col items-center">
