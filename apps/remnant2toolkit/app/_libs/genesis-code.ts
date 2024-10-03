a12050bbc04ebd0c1c28369e96eb76b8317bd722
Josh Payette <hello@joshpayette.dev>
2024-10-03 08:45:20 -0400

    Fixed undefined error

--- apps/remnant2toolkit/app/_libs/genesis-code.ts
+++ apps/remnant2toolkit/app/_libs/genesis-code.ts
@@ -6,9 +6,10 @@ export function calculateCode(ts: number): string {
   };
 
   const index = Math.floor((ts - seeds.date) / 3600);
+  const offset = seeds.offsets[index % 8] ?? 0;
   const code =
     ((index * seeds.code + 2 ** 31) % 2 ** 32) -
     2 ** 31 +
-    seeds.offsets[index % 8];
+    offset;
   return code.toString().slice(-4).padStart(4, '0');
 }
