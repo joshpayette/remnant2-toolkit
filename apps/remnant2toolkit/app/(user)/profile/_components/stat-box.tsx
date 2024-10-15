47f681fb76ac8bf8178075be57fba00e75a2e5b0
Josh Payette <hello@joshpayette.dev>
2024-10-14 21:46:47 -0400

    Updated profile stats in the header

--- apps/remnant2toolkit/app/(user)/profile/_components/stat-box.tsx
+++ apps/remnant2toolkit/app/(user)/profile/_components/stat-box.tsx
@@ -2,11 +2,12 @@ import { cn } from '@repo/ui';
 
 interface Props {
   stat: { name: string; value: number; unit?: string };
+  icon?: React.ReactNode;
   index: number;
   footer?: React.ReactNode;
 }
 
-export function StatBox({ stat, index, footer }: Props) {
+export function StatBox({ stat, icon, index, footer }: Props) {
   return (
     <div
       key={stat.name}
@@ -19,6 +20,7 @@ export function StatBox({ stat, index, footer }: Props) {
         'border-surface-solid/5 flex flex-col items-center justify-between border-t px-4 py-6 sm:px-6',
       )}
     >
+      <div className="">{icon ?? null}</div>
       <p className="text-sm font-medium leading-6 text-gray-400">{stat.name}</p>
       <p className="mt-2 flex items-baseline justify-center gap-x-2">
         <span className="text-surface-solid text-4xl font-semibold tracking-tight">
