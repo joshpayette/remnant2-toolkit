ed53c5b0cc4e1394813dff817ff5d37c1533e371
Josh Payette <hello@joshpayette.dev>
2024-10-23 22:36:24 -0400

    Made some final style touches

--- packages/ui/src/common/filters/filter-listbox/multi-value-label.tsx
+++ packages/ui/src/common/filters/filter-listbox/multi-value-label.tsx
@@ -16,7 +16,7 @@ export function MultiValueLabel(props: MultiValueGenericProps<FilterOption>) {
   return (
     <div
       className={cn(
-        'ui-bg-black ui-text-xs ui-text-surface-solid ui-flex ui-flex-row ui-justify-start ui-items-center ui-p-1',
+        'ui-bg-black ui-text-xs ui-text-surface-solid ui-flex ui-flex-row ui-justify-start ui-items-center ui-p-1 ui-mb-1',
       )}
     >
       <div className="ui-bg-black ui-flex ui-items-center ui-justify-center mr-0.5">
