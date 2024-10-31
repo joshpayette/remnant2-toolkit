6d9ab83448b4547ca43f95dc9f884ce20cf8d9da
Josh Payette <hello@joshpayette.dev>
2024-10-31 18:17:42 -0400

    Added new border color for filters not applied

--- packages/ui/src/common/filters/filters-container/index.tsx
+++ packages/ui/src/common/filters/filters-container/index.tsx
@@ -24,32 +24,39 @@ export function FiltersContainer({
   return (
     <Disclosure defaultOpen>
       {({ open }) => (
-        <div className="w-full">
-          <div className="border-b-primary-500 flex w-full flex-row items-end justify-end border-b py-2">
-            <div className="w-full pr-4">{searchInput}</div>
+        <div className="ui-w-full">
+          <div className="ui-border-b-primary-500 ui-flex ui-w-full ui-flex-row ui-items-end ui-justify-end ui-border-b ui-py-2">
+            <div className="ui-w-full ui-pr-4">{searchInput}</div>
             <Disclosure.Button as={BaseButton}>
-              <FilterIcon className="h-4 w-4" />
+              <FilterIcon className="ui-h-4 ui-w-4" />
               {open ? 'Hide' : 'Show'}
             </Disclosure.Button>
           </div>
           <Disclosure.Panel
             className={cn(
-              'border-primary-500 mt-2 w-full border bg-gray-950 p-4',
+              'ui-border-primary-500 ui-mt-2 ui-w-full ui-border ui-bg-gray-950 ui-p-4',
               areAnyFiltersActive &&
-                'border-accent1-300 shadow-accent1-600 shadow-xl',
+                'ui-border-accent1-300 ui-shadow-accent1-600 ui-shadow-xl',
+              !areFiltersApplied &&
+                'ui-border-secondary-300 ui-shadow-secondary-600 ui-shadow-xl',
             )}
           >
             {children}
-            <div className="mt-4 flex justify-end">
-              <BaseButton className="mr-2" color="red" onClick={onClearFilters}>
+            <div className="ui-mt-4 ui-flex ui-justify-end">
+              <BaseButton
+                className="ui-mr-2"
+                color="red"
+                onClick={onClearFilters}
+              >
                 Clear
               </BaseButton>
               <BaseButton
-                className={cn(!areFiltersApplied && 'animate-pulse')}
+                className={cn(!areFiltersApplied && 'ui-animate-pulse')}
                 color="green"
+                disabled={areFiltersApplied}
                 onClick={onApplyFilters}
               >
-                Apply
+                Apply Filters
               </BaseButton>
             </div>
           </Disclosure.Panel>
