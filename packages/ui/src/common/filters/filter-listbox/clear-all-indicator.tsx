6613b0af6b3728566d6c6233e69a2810629be6bd
Josh Payette <hello@joshpayette.dev>
2024-10-31 17:11:39 -0400

    Pushed fix for filterbox behavior

--- packages/ui/src/common/filters/filter-listbox/clear-all-indicator.tsx
+++ packages/ui/src/common/filters/filter-listbox/clear-all-indicator.tsx
@@ -1,18 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable react/button-has-type */
+
 import type { ClearIndicatorProps } from 'react-select';
 import { components } from 'react-select';
 import type { FilterOption } from './types';
 
 export function ClearIndicator(props: ClearIndicatorProps<FilterOption, true>) {
   return (
-    <button
-      onClick={() => {
-        // @ts-expect-error Need to pass a custom prop to the option component
-        props.selectProps.onResetOptions();
+    <components.ClearIndicator
+      {...props}
+      innerProps={{
+        ...props.innerProps,
+        onClick: (e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          // @ts-expect-error Need to pass a custom prop to the option component
+          props.selectProps.onResetOptions();
+        },
+        onTouchEnd: (e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          // @ts-expect-error Need to pass a custom prop to the option component
+          props.selectProps.onResetOptions();
+        },
       }}
-    >
-      <components.ClearIndicator {...props} />
-    </button>
+    />
   );
 }
--- packages/ui/src/common/filters/filter-listbox/multi-value-remove.tsx
+++ packages/ui/src/common/filters/filter-listbox/multi-value-remove.tsx
@@ -4,18 +4,29 @@ import type { FilterOption } from './types';
 
 export function MultiValueRemove(props: MultiValueRemoveProps<FilterOption>) {
   return (
-    <components.MultiValueRemove {...props}>
-      <button
-        className="ui-bg-red-900 ui-w-3 ui-h-6 ui-flex ui-items-center ui-justify-center ui-mr-2 ui-p-1 ui-mb-1"
-        onClick={() => {
+    <components.MultiValueRemove
+      {...props}
+      innerProps={{
+        ...props.innerProps,
+        onClick: (e) => {
+          e.stopPropagation();
+          e.preventDefault();
           // @ts-expect-error Need to pass a custom prop to the option component
           // eslint-disable-next-line @typescript-eslint/no-unsafe-call
           props.selectProps.onOptionChange('default', props.data.value);
-        }}
-        type="button"
-      >
+        },
+        onTouchEnd: (e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          // @ts-expect-error Need to pass a custom prop to the option component
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          props.selectProps.onOptionChange('default', props.data.value);
+        },
+      }}
+    >
+      <div className="ui-bg-red-900 ui-w-3 ui-h-6 ui-flex ui-items-center ui-justify-center ui-mr-2 ui-p-1 ui-mb-1">
         <XIcon className="ui-w-2 ui-h-2 ui-text-surface-solid" />
-      </button>
+      </div>
     </components.MultiValueRemove>
   );
 }
