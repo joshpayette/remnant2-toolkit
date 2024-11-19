82ef595beacc6ef2ef3f2819fde92fa7385da505
Josh Payette <hello@joshpayette.dev>
2024-11-19 15:49:54 -0500

    Added moderation webhooks

--- TODO.md
+++ TODO.md
@@ -3,5 +3,3 @@
 - Add CHANGELOG: `Linked Builds link removed from user profiles. This can still be found by manually adding /linked-builds at the end of your profile link. This feature is no longer officially supported.`
 - Add edit logic to build collections page
   - Need to be able to change build name and description (probably just a dialog)
-- Add moderation hooks around build names and descriptions
-- Ensure build collection descriptions limited to 500 characters (constant already set)
--- apps/remnant2toolkit/app/(user)/profile/[profileId]/collections/_actions/create-build-collection.ts
+++ apps/remnant2toolkit/app/(user)/profile/[profileId]/collections/_actions/create-build-collection.ts
@@ -1,8 +1,11 @@
 'use server';
 
 import { prisma } from '@repo/db';
+import { urlNoCache } from '@repo/utils';
 
+import { badWordFilter } from '@/app/_libs/bad-word-filter';
 import { type ErrorResponse } from '@/app/_types/error-response';
+import { MAX_COLLECTION_DESCRIPTION_LENGTH } from '@/app/(builds)/_constants/max-build-description-length';
 import { getSession } from '@/app/(user)/_auth/services/sessionService';
 import type { BuildCollectionWithBuilds } from '@/app/(user)/profile/[profileId]/collections/_types/build-collection-with-builds';
 
@@ -24,10 +27,15 @@ export async function createBuildCollection({
   }
 
   try {
-    const collection = await prisma.buildCollection.create({
+    const cleanCollectionName = badWordFilter.clean(collectionName);
+    const cleanCollectionDescription = badWordFilter.clean(
+      collectionDescription.slice(0, MAX_COLLECTION_DESCRIPTION_LENGTH),
+    );
+
+    const createdCollection = await prisma.buildCollection.create({
       data: {
-        name: collectionName,
-        description: collectionDescription,
+        name: cleanCollectionName,
+        description: cleanCollectionDescription,
         createdById: session.user.id,
         BuildsToBuildCollections: {
           create: buildIds.map((id) => ({
@@ -46,10 +54,34 @@ export async function createBuildCollection({
       },
     });
 
+    const buildLink = `${urlNoCache(
+      `https://remnant2toolkit.com/profile/${session.user.id}/collections/${createdCollection.id}`,
+    )}`;
+
+    const shouldSendWebhook = process.env.WEBHOOK_DISABLED !== 'true';
+    if (shouldSendWebhook) {
+      const newBuildCollectionParams = {
+        content: `New build collection created! ${buildLink}`,
+      };
+      const newBuildWebhookResponse = await fetch(
+        `${process.env.WEBHOOK_MOD_QUEUE}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newBuildCollectionParams),
+        },
+      );
+      if (!newBuildWebhookResponse.ok) {
+        console.error('Error in sending build moderation webhook to Discord!');
+      }
+    }
+
     return {
       message: 'Build collection created successfully.',
       collection: {
-        ...collection,
+        ...createdCollection,
         builds: buildIds.map((id) => ({ id })),
       },
     };
