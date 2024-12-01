800929442f822dc2d9766a1cc6eb108168b45e3a
Josh Payette <hello@joshpayette.dev>
2024-12-01 12:29:16 -0500

    Installed next 15

--- packages/ygt-toolkits/src/configs/remnant2toolkit.ts
+++ packages/ygt-toolkits/src/configs/remnant2toolkit.ts
@@ -1,8 +1,18 @@
 import type { ToolkitConfig } from '../types/toolkit-config';
 
 export const CONFIG: ToolkitConfig = {
+  NAV_ITEMS: {
+    home: {
+      label: 'Home',
+      description:
+        'Remnant 2 is a collection of toolkits for some of your favorite games.',
+      href: '/',
+      icon: null,
+    },
+  },
   SITE_TITLE: 'Remnant2Toolkit.com',
-  SITE_DESCRIPTION: 'A collection of toolkits for some of your favorite games.',
+  SITE_DESCRIPTION:
+    'Remnant 2 item tracking, build sharing, item database, and more!',
   URL_CHANGELOG: undefined,
   URL_DISCORD_INVITE: undefined,
   URL_IMAGE_BASE: `https://dbv3oah7mgkxy.cloudfront.net/remnant2toolkit`,
