f99564f89c60069a08b4665ba8ca6c5ae9f97332
Josh Payette <hello@joshpayette.dev>
2026-02-28 15:12:18 -0500

    added nuqsadapter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { type Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import { SessionProvider } from 'next-auth/react';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { ToastContainer } from 'react-toastify';
 
 import { showNotificationsFlag } from '@/app/_constants/feature-flag';
@@ -44,23 +45,25 @@ export default async function Layout({
       >
         <PreloadResources />
         <SessionProvider session={session}>
-          <ThemeSelection>
-            <ToastContainer pauseOnFocusLoss={false} theme="dark" />
-            <div className="flex h-full w-full max-w-[1500px] grow flex-col items-start justify-start">
-              <header className="w-full">
-                <Navbar showNotifications={showNotifications} />
-              </header>
+          <NuqsAdapter>
+            <ThemeSelection>
+              <ToastContainer pauseOnFocusLoss={false} theme="dark" />
+              <div className="flex h-full w-full max-w-[1500px] grow flex-col items-start justify-start">
+                <header className="w-full">
+                  <Navbar showNotifications={showNotifications} />
+                </header>
 
-              <main className="mt-[80px] flex h-full min-h-screen w-full grow flex-col items-center justify-start p-4 pt-0">
-                <GlobalActionButtons
-                  username={session?.user?.name || 'Unknown User'}
-                />
-                {children}
-              </main>
-            </div>
+                <main className="mt-[80px] flex h-full min-h-screen w-full grow flex-col items-center justify-start p-4 pt-0">
+                  <GlobalActionButtons
+                    username={session?.user?.name || 'Unknown User'}
+                  />
+                  {children}
+                </main>
+              </div>
 
-            <Footer />
-          </ThemeSelection>
+              <Footer />
+            </ThemeSelection>
+          </NuqsAdapter>
         </SessionProvider>
         <Analytics />
       </body>
