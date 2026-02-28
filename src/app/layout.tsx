9e2c15456ea15cc39aacdbbd83276fe93bae1012
Josh Payette <hello@joshpayette.dev>
2026-02-28 15:11:08 -0500

    removed unnecessary RootLayout abstraction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,28 @@ import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import { type Viewport } from 'next';
+import { Inter } from 'next/font/google';
+import { SessionProvider } from 'next-auth/react';
+import { ToastContainer } from 'react-toastify';
 
 import { showNotificationsFlag } from '@/app/_constants/feature-flag';
 import { Navbar } from '@/app/navbar';
 import { Footer } from '@/components/footer';
 import { auth } from '@/lib/auth';
 import { GlobalActionButtons } from '@/ui/common/global-action-buttons';
-import { RootLayout } from '@/ui/common/root-layout';
+import { PreloadResources } from '@/ui/common/preload-resources';
+import ThemeSelection from '@/ui/common/theme-select/theme-selection';
+import { cn } from '@/utils/classnames';
+
+export { metadata } from './metadata';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const viewport: Viewport = {};
-export { metadata } from './metadata';
 
 export default async function Layout({
   children,
@@ -22,27 +34,36 @@ export default async function Layout({
   const showNotifications = await showNotificationsFlag();
 
   return (
-    <RootLayout
-      alertBanner={
-        null
-        // <AlertBanner localStorageKey="one-year-anniversary">
-        //   December 17th marks our one year anniversary! Want to win a DLC of
-        //   choice?{' '}
-        //   <BaseTextLink
-        //     href="https://www.reddit.com/r/remnantgame/comments/1hgb5dv/remnant_2_toolkit_one_year_anniversary_giveaway/"
-        //     target="_blank"
-        //   >
-        //     Check out this Reddit thread for a chance to win!
-        //   </BaseTextLink>
-        // </AlertBanner>
-      }
-      footer={<Footer />}
-      navbar={<Navbar showNotifications={showNotifications} />}
-      trackers={<Analytics />}
-    >
-      <GlobalActionButtons username={session?.user?.name || 'Unknown User'} />
-      {/* <RemnantOverseerCTA /> */}
-      {children}
-    </RootLayout>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          'relative flex min-h-fit flex-col items-center justify-start',
+          inter.className
+        )}
+      >
+        <PreloadResources />
+        <SessionProvider session={session}>
+          <ThemeSelection>
+            <ToastContainer pauseOnFocusLoss={false} theme="dark" />
+            <div className="flex h-full w-full max-w-[1500px] grow flex-col items-start justify-start">
+              <header className="w-full">
+                <Navbar showNotifications={showNotifications} />
+              </header>
+
+              <main className="mt-[80px] flex h-full min-h-screen w-full grow flex-col items-center justify-start p-4 pt-0">
+                <GlobalActionButtons
+                  username={session?.user?.name || 'Unknown User'}
+                />
+                {children}
+              </main>
+            </div>
+
+            <Footer />
+          </ThemeSelection>
+        </SessionProvider>
+        <Analytics />
+      </body>
+    </html>
   );
 }
